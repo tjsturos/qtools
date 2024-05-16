38de089c504b84a8cbedd78ab58e819d26c226e2
Base Dev <gitlab.dollop533@passmail.net>
2024-05-15 23:04:14 -0800

    these files don't exist

--- scripts/remove-docker.sh
+++ scripts/remove-docker.sh
@@ -7,16 +7,13 @@ mv .config/config.yml node/.config/config.yml
 
 docker compose down
 
-docker system prune -a
+docker system prune -a -y
 apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli docker-compose-plugin
 apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-compose-plugin
 
 rm -rf /var/lib/docker /etc/docker
-rm /etc/apparmor.d/docker
 groupdel docker
 rm -rf /var/run/docker.sock
 rm -rf /var/lib/containerd
-rm /usr/local/bin/docker-compose
-rm -r /etc/docker
 rm -r ~/.docker
 rm /usr/share/keyrings/docker-archive-keyring.gpg
