fdfd13bc0c13a2a04dc574e6e0ca5a21354fdb28
Base Dev <gitlab.dollop533@passmail.net>
2024-05-16 08:18:21 -0800

    update purge process

--- scripts/purge.sh
+++ scripts/purge.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-STORED_PATH="$(cat ~/saved-path)"
-PATH=$STORED_PATH
-log "Loading stored path: $STORED_PATH"
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Stop the ceremonyclient service
 systemctl stop ceremonyclient.service
 
 # make a backup of the keys
-source ./backup/local-backup.sh
+source $SCRIPT_DIR/backup/local-backup.sh
 
 # remove old node code
 rm -rf /root/ceremonyclient
@@ -19,11 +15,12 @@ rm /root/go/bin/node
 # reinstall
 git clone https://github.com/QuilibriumNetwork/ceremonyclient.git
 
+cd ceremonyclient/node
+GOEXPERIMENT=arenas go install ./...
+systemctl start ceremonyclient.service
+
+wait 60
 
 source $SCRIPT_DIR/backup/restore-backup.sh
-cd ceremonyclient/node
 
 
-GOEXPERIMENT=arenas go install ./...
-
-systemctl start ceremonyclient.service
