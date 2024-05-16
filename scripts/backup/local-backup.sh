49e83cb7d4879d6d17121a5e2a8aff5e34815dd6
Base Dev <gitlab.dollop533@passmail.net>
2024-05-15 23:00:09 -0800

    remove docker

--- scripts/backup/local-backup.sh
+++ scripts/backup/local-backup.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 BACKUP_DIR=~/quil-backup
-# Check if the files exist
-if [ -f ~/ceremonyclient/node/keys.yml ] && [ -f ~/ceremonyclient/node/config.yml ]; then
-    # Create the directory if it doesn't exist
-    mkdir -p ~/quil-backup/
+# Create the directory if it doesn't exist
+mkdir -p $BACKUP_DIR
 
+if [ -f ~/ceremonyclient/node/.config/keys.yml ] && [ -f ~/ceremonyclient/node/.config/config.yml ]; then
+    # Check if the files exist (node)
     # Copy the files
-    cp ~/ceremonyclient/node/keys.yml $BACKUP_DIR
-    cp ~/ceremonyclient/node/config.yml $BACKUP_DIR
+    cp ~/ceremonyclient/node/.config/keys.yml $BACKUP_DIR
+    cp ~/ceremonyclient/node/.config/config.yml $BACKUP_DIR
 
-    echo "Quilibrium key.yml and config.yml backed up to "
+    echo "Quilibrium ~/ceremonyclient/node/.config/keys.yml and ~/ceremonyclient/node/.config/config.yml backed up to "
+elif [ -f ~/ceremonyclient/.config/keys.yml ] && [ -f ~/ceremonyclient/.config/config.yml ]; then
+    # Check if the files exist (docker)
+    # Copy the files
+    cp ~/ceremonyclient/.config/keys.yml $BACKUP_DIR
+    cp ~/ceremonyclient/.config/config.yml $BACKUP_DIR
+
+    echo "Quilibrium ~/ceremonyclient/.config/keys.yml and ~/ceremonyclient/.config/config.yml backed up to "
 else
     echo "One or both of the files keys.yml and config.yml do not exist."
 fi
