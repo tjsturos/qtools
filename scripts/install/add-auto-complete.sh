b00a7ea52643c7d649ea6be9fe6a9befe006e13e
Base Dev <gitlab.dollop533@passmail.net>
2024-05-18 12:44:30 -0800

    fix add-autocomplete

--- scripts/install/add-auto-complete.sh
+++ scripts/install/add-auto-complete.sh
@@ -5,5 +5,5 @@ apt-get install bash-completion
 append_to_file ~/.bashrc "source /etc/profile.d/bash_completion.sh"
 
 source /etc/profile.d/bash_completion.sh
-
-complete -W 'complete-install create-qtools-symlink install-go install-node-binary install-qclient-binary modify-config setup-cron setup-firewall purge remove-docker update-node' qtools
+append_to_file ~/.bashrc "complete -W 'complete-install create-qtools-symlink install-go install-node-binary install-qclient-binary modify-config setup-cron setup-firewall purge remove-docker update-node' qtools"
+source ~/.bashrc
