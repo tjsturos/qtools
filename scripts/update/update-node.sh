64a1f3015aaab226a324b8b07992026f76665abe
Base Dev <gitlab.dollop533@passmail.net>
2024-05-20 22:07:35 -0800

    change update logic

--- scripts/update/update-node.sh
+++ scripts/update/update-node.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 cd $QUIL_PATH
 
-version_gt() {
-    test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1";
-}
-
-REMOTE_VERSION=$(git -c 'versionsort.suffix=-' \
-    ls-remote --exit-code --refs --sort='version:refname' --tags <repository> '*.*.*' \
-    | tail --lines=1 \
-    | cut --delimiter='/' --fields=3  | cut -b 2-7)
-
 CURRENT_VERSION=$(git tag --sort=committerdate | grep -E '[0-9]' | tail -1 | cut -b 2-7)
 
 # only run update commands if the remote version is newer than local
-if version_gt "$REMOTE_VERSION" "$CURRENT_VERSION"; then
+if [ "$(git rev-list HEAD..origin/main --count)" -gt 0 ]; then
     systemctl stop ceremonyclient.service
     git fetch origin
     git merge origin
@@ -25,6 +16,8 @@ if version_gt "$REMOTE_VERSION" "$CURRENT_VERSION"; then
     qtools install-qclient-binary
 
     systemctl start ceremonyclient.service
+    NEW_VERSION=$(git tag --sort=committerdate | grep -E '[0-9]' | tail -1 | cut -b 2-7)
+    log "Installing new version: $CURRENT_VERSION->$NEW_VERSION"
 else
-    echo "No new Quilibrium version found: $REMOTE_VERSION vs. $CURRENT_VERSION"
+    log "No new Quilibrium version found (existing: $CURRENT_VERSION)"
 fi
