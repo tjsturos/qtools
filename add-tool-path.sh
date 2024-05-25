41c5309f6a4ff6118150cb007e022027779e040a
Base Dev <gitlab.dollop533@passmail.net>
2024-05-25 14:56:13 -0800

    do not readd if existing

--- add-tool-path.sh
+++ add-tool-path.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-if [ -z "$QTOOLS_PATH" ]; then
+append_to_file() {
+    FILE="$1"
+    CONTENT="$2"
+
+    if ! grep -qFx "$CONTENT" $FILE; then
+        log "Adding $CONTENT to $FILE"
+        echo "$CONTENT" >> $FILE
+    else
+        log "$CONTENT already found in $FILE. Skipping."
+    fi
+}
+
   # Define TOOL_PATH (example path, change as needed)
-  export QTOOLS_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export QTOOLS_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-  # Add TOOL_PATH definition to ~/.bashrc
-  echo "export QTOOLS_PATH=\"$QTOOLS_PATH\"" >> ~/.bashrc
+append_to_file ~/.bashrc "export QTOOLS_PATH=\"$QTOOLS_PATH\""
 
-  # Reload ~/.bashrc to apply changes
-  source ~/.bashrc
-fi
+# Reload ~/.bashrc to apply changes
+source ~/.bashrc
