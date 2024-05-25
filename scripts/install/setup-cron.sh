620003dad98ae8ab10d873b481a8ec6e2ac312c4
Base Dev <gitlab.dollop533@passmail.net>
2024-05-25 14:39:11 -0800

    update cron-tab

--- scripts/install/setup-cron.sh
+++ scripts/install/setup-cron.sh
@@ -2,10 +2,10 @@
 # The aim of this is to get the current cron tasks file, add the line to run the on-start.sh script every reboot.
 FILE_CRON=$QTOOLS_PATH/cron
 
-append_to_file $FILE_CRON "GOROOT=/usr/local/go"
-append_to_file $FILE_CRON "GOPATH=/root/go"
-append_to_file $FILE_CRON "PATH=/root/go/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
-append_to_file $FILE_CRON "QTOOLS_PATH=$QTOOLS_PATH"
+append_to_file $FILE_CRON "export GOROOT=/usr/local/go"
+append_to_file $FILE_CRON "export GOPATH=/root/go"
+append_to_file $FILE_CRON "export PATH=/root/go/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+append_to_file $FILE_CRON "export QTOOLS_PATH=$QTOOLS_PATH"
 append_to_file $FILE_CRON "1 0 * * * qtools make-backup"
 append_to_file $FILE_CRON "1 */3 * * * qtools update-qtools"
 append_to_file $FILE_CRON "2 */3 * * * qtools update-node"
@@ -16,10 +16,10 @@ crontab $FILE_CRON
 # Finally remove the file we initially created as it's not needed.
 remove_file $FILE_CRON
 
-expected_output="GOROOT=/usr/local/go
-GOPATH=/root/go
-PATH=/root/go/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-QTOOLS_PATH=/root/quilibrium-node-setup
+expected_output="export GOROOT=/usr/local/go
+export GOPATH=/root/go
+export PATH=/root/go/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export QTOOLS_PATH=$QTOOLS_PATH
 1 0 * * * qtools make-backup
 1 */3 * * * qtools update-qtools
 2 */3 * * * qtools update-node"
