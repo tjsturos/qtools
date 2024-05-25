b0072ce282dba9a9169a018abf2cf8474e10ac12
Base Dev <gitlab.dollop533@passmail.net>
2024-05-25 01:31:07 -0800

    update to make sure that unzip is installed

--- scripts/install/import-store.sh
+++ scripts/install/import-store.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+install_package unzip zip
 rm -r $QUIL_NODE_PATH/.config/store 
 wget -qO- https://snapshots.cherryservers.com/quilibrium/store.zip > /tmp/store.zip 
 unzip -j -o /tmp/store.zip -d $QUIL_NODE_PATH/.config/store 
