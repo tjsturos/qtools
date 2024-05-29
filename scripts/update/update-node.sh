5febb7ca8a2a9c1b07ad13c51038a91f601f3ad5
Base Dev <gitlab.dollop533@passmail.net>
2024-05-29 10:56:46 -0800

    detect changes in release branch

--- scripts/update/update-node.sh
+++ scripts/update/update-node.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 cd $QUIL_PATH
 
-git pull
+#!/bin/bash
+
+# Fetch the latest changes from the remote repository
+git fetch origin
+
+# Check if there are any new commits on the remote release branch
+LOCAL=$(git rev-parse release)
+REMOTE=$(git rev-parse origin/release)
+
 git checkout release
+if [ $LOCAL != $REMOTE ]; then
+    echo "Release branch has been updated. Pulling changes and restarting service."
+    
+    # Pull the latest changes from the remote repository
+    git pull
+    
+    # Start qtools
+    qtools start
+    
+    # Force a restart
+    qtools restart
+else
+    echo "Release branch is up-to-date. No restart required."
+    
 
-qtools start
-
-# force a restart for when the version doesn't change, but the binary does
-qtools restart
+    # Start qtools without restart
+    qtools start
+fi
