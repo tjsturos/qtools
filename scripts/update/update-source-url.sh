9250db0a16ae62a81acd86962db655d5901498fc
Base Dev <gitlab.dollop533@passmail.net>
2024-05-29 16:38:08 -0800

    just set to fixed URL

--- scripts/update/update-source-url.sh
+++ scripts/update/update-source-url.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-GIT_SOURCE="${1:-$SOURCE_URL}"
 
 if [ -d "$QUIL_PATH" ]; then
     cd $QUIL_PATH
-    git remote set-url origin $GIT_SOURCE
+    git remote set-url origin $SOURCE_URL
 else
     log "No directory "$QUIL_PATH" exists."
     exit 1
