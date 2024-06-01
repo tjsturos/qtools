7ba23ae2987c12a4eb124b35fd5a38aa0f4d9b19
Base Dev <gitlab.dollop533@passmail.net>
2024-06-01 10:04:47 -0800

    move code to appropriate file

--- scripts/grpc/get-peer-id.sh
+++ scripts/grpc/get-peer-id.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "$(grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNodeInfo | grep -oP '"peerId":\s*"\K[^"]+')"
+PEER_ID="$(grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNodeInfo | grep -oP '"peerId":\s*"\K[^"]+')"
+
+# Check if a Peer ID was found
+if [ ! -n "$PEER_ID" ]; then
+    # Run the node-get-peer-id script with the extracted Peer ID
+    PEER_ID="$(source $QTOOLS_PATH/scripts/node-commands/node-get-peer-id.sh)"
+
+    if [ -n "$PEER_ID" ]; then
+        echo "$PEER_ID"
+    fi
+fi
--- scripts/grpc/get-peer-info.sh
+++ scripts/grpc/get-peer-info.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-# Run the grpcurl command and capture its output
-OUTPUT=$(grpcurl -plaintext -max-msg-sz 5000000 localhost:8337 quilibrium.node.node.pb.NodeService.GetPeerInfo)
-
-# Extract the Peer ID from the output
-PEER_ID=$(echo "$OUTPUT" | grep -oP 'Peer ID: \K.*')
-
-# Check if a Peer ID was found
-if [ ! -n "$PEER_ID" ]; then
-    # Run the node-get-peer-id script with the extracted Peer ID
-    PEER_ID="$(source $QTOOLS_PATH/scripts/node-commands/node-get-peer-id.sh)"
-
-    if [ -n "$PEER_ID" ]; then
-        echo "$PEER_ID"
-    fi
-fi
+grpcurl -plaintext -max-msg-sz 5000000 localhost:8337 quilibrium.node.node.pb.NodeService.GetPeerInfo
