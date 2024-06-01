bf9cb7a9f9f692250757c15185ab6555ddab30f7
Base Dev <gitlab.dollop533@passmail.net>
2024-05-31 23:20:33 -0800

    add message

--- scripts/install/install-cron.sh
+++ scripts/install/install-cron.sh
@@ -2,6 +2,8 @@
 # The aim of this is to get the current cron tasks file, add the line to run the on-start.sh script every reboot.
 FILE_CRON=$QTOOLS_PATH/cron
 
+remove_file $FILE_CRON
+
 append_to_file $FILE_CRON "GOROOT=$GOROOT" false
 append_to_file $FILE_CRON "GOPATH=$GOPATH" false
 append_to_file $FILE_CRON "PATH=\$GOPATH/bin:\$GOROOT/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin" false
@@ -17,9 +19,11 @@ actual_output="$(crontab -l)"
 
 if [[ "$actual_output" == "$FILE_CRON" ]]; then
   echo "The crontab was successfully updated."
+  remove_file $FILE_CRON
   exit 0
 else
   actual_file="actual.txt"
+  echo "The crontab was contains some errors:"
   echo "$actual_output" > $actual_file
   colordiff -u $FILE_CRON $actual_file
   remove_file $FILE_CRON
