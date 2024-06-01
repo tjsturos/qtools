79efa7c592eb97587e047017b4a79cbfce83fa6a
Base Dev <gitlab.dollop533@passmail.net>
2024-06-01 00:18:58 -0800

    change to grpcurl command

--- scripts/grpc/get-node-info.sh
+++ scripts/grpc/get-node-info.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cd $QUIL_NODE_PATH && GOEXPERIMENT=arenas go run ./... -node-info
-# grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNodeInfo
+grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNodeInfo
