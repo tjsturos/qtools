66436da0a2b520e68a48b926676d7cf8d0755afd
Base Dev <gitlab.dollop533@passmail.net>
2024-06-08 22:29:37 +0300

    update get reward balance

--- scripts/node-commands/node-get-reward-balance.sh
+++ scripts/node-commands/node-get-reward-balance.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # prints out balance in QUILs
-$QUIL_BIN -balance
+input="$($QUIL_BIN -balance)"
+
+unclaimed_balance=$(echo "$input" | grep "Unclaimed balance" | awk -F ": " '{print $2}' | awk '{print $1}')
+
+echo "$unclaimed_balance"
