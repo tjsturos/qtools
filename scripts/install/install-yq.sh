2d5df2786b466f59e3a0c7dc853742e7917e295e
Base Dev <gitlab.dollop533@passmail.net>
2024-06-11 00:04:13 +0300

    add prefix to version

--- scripts/install/install-yq.sh
+++ scripts/install/install-yq.sh
@@ -1,5 +1,5 @@
 
-VERSION=4.2.0
+VERSION=v4.2.0
 BINARY=yq_linux_amd64
 wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - |\
   tar xz && mv ${BINARY} /usr/bin/yq
