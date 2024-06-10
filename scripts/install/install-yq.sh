0cc42037eb7e997170cbad8055e81501ec645a90
Base Dev <gitlab.dollop533@passmail.net>
2024-06-11 00:16:36 +0300

    update file extraction

--- scripts/install/install-yq.sh
+++ scripts/install/install-yq.sh
@@ -3,6 +3,6 @@ VERSION=v4.2.0
 BINARY=yq_linux_amd64
 COMPRESSED_FILENAME=${BINARY}.tar.gz
 wget -q https://github.com/mikefarah/yq/releases/download/${VERSION}/${COMPRESSED_FILENAME} 
-tar xz $COMPRESSED_FILENAME &> /dev/null
+tar -xzf $COMPRESSED_FILENAME &> /dev/null
 mv $BINARY /usr/bin/yq
-rm $COMPRESSED_FILENAME
+remove_file $COMPRESSED_FILENAME false
