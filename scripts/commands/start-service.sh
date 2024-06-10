803f5bd70828b8ece9fad258738c72bee8685a33
Base Dev <gitlab.dollop533@passmail.net>
2024-06-11 01:41:26 +0300

    update for loop

--- scripts/commands/start-service.sh
+++ scripts/commands/start-service.sh
@@ -16,11 +16,11 @@ IS_LINKED="$(yq e '.settings.slave' $QTOOLS_CONFIG_FILE)"
 if [ "$IS_LINKED" == "true" ]; then
     MAIN_PID="$(systemctl status $QUIL_SERVICE_NAME | grep 'Main PID' | awk '{print $3}')"
     # if is linked, then start the secondary processes
-    CORE_COUNT=$(get_processor_count)
+    CORE_COUNT=$((get_processor_count))
     log Node parent ID: $MAIN_PID;
 
-    for I in $( eval echo {1..$length} ); do
-        echo "NODE_ARGS=\"--core=$(expr $I) --parent-process=$NODE_PID\"" > $PROCESS_DIR/$I
-        systemctl start $QUIL_SERVICE_NAME@$I.service
+    for ((i = 1 ; i <= $CORE_COUNT ; i++)); do
+        echo "NODE_ARGS=\"--core=$(expr $i) --parent-process=$NODE_PID\"" > $PROCESS_DIR/$i
+        systemctl start $QUIL_SERVICE_NAME@$i.service
     done
 fi
