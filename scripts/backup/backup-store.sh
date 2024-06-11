0d4a53d683674c93da0aa917b95082a4a2214aa4
Base Dev <gitlab.dollop533@passmail.net>
2024-06-11 18:00:42 +0300

    don't run command if SSH connection cannot be made

--- scripts/backup/backup-store.sh
+++ scripts/backup/backup-store.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 LOCAL_HOST_NAME=$(hostname)
 REMOTE_DIR="/root/backups/$LOCAL_HOST_NAME/"
-ssh backup-server "mkdir -p $REMOTE_DIR"
-rsync -avz --ignore-existing -e ssh "$QUIL_NODE_PATH/.config" "backup-server:$REMOTE_DIR"
+SSH_ALIAS="backup-server"
+
+ssh -q -o BatchMode=yes -o ConnectTimeout=5 $SSH_ALIAS exit
+
+if [ $? -ne 0 ]; then
+  echo "SSH alias $SSH_ALIAS does not exist or is not reachable."
+  exit 1
+fi
+
+ssh $SSH_ALIAS "mkdir -p $REMOTE_DIR"
+rsync -avz --ignore-existing -e ssh "$QUIL_NODE_PATH/.config" "$SSH_ALIAS:$REMOTE_DIR"
