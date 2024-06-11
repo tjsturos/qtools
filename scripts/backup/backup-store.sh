92a3e0f9e1a356262d48511862434ac8cbe9d836
Base Dev <gitlab.dollop533@passmail.net>
2024-06-11 17:56:28 +0300

    create direcory on backup server if it doesn't exist

--- scripts/backup/backup-store.sh
+++ scripts/backup/backup-store.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 LOCAL_HOST_NAME=$(hostname)
 REMOTE_DIR="/root/backups/$LOCAL_HOST_NAME/"
+ssh backup-server "mkdir -p $REMOTE_DIR"
 rsync -avz --ignore-existing -e ssh "$QUIL_NODE_PATH/.config" "backup-server:$REMOTE_DIR"
