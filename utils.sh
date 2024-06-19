db06934550157086322e8cd143a9f925d91eea50
Base Dev <gitlab.dollop533@passmail.net>
2024-06-19 22:29:13 +0300

    update to correct insert syntax

--- utils.sh
+++ utils.sh
@@ -131,7 +131,7 @@ remove_lines_matching_pattern() {
 
 set_release_version() {
     local new_version="$1"
-    yq -i '.release_version = strenv(new_version)' $QTOOLS_CONFIG_FILE
+    yq -i e '.release_version = strenv(new_version)' $QTOOLS_CONFIG_FILE
 }
 
 fetch_release_version() {
@@ -142,7 +142,7 @@ fetch_release_version() {
 
 set_current_version() {
     local current_version="$1"
-    yq -i '.current_version = strenv(current_version)' $QTOOLS_CONFIG_FILE
+    yq -i e '.current_version = strenv(current_version)' $QTOOLS_CONFIG_FILE
 }
 
 get_current_version() {
