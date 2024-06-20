6423458e87329464f050f73569d5f741c4396b73
Base Dev <gitlab.dollop533@passmail.net>
2024-06-20 15:42:11 +0300

    update service to look for

--- utils.sh
+++ utils.sh
@@ -144,7 +144,14 @@ set_current_version() {
 }
 
 get_current_version() {
-    local CURRENT_VERSION=$(systemctl status $QUIL_SERVICE_NAME | grep -oP "\-([0-9]+\.)+([0-9]+)\-" | head -n 1 | tr -d 'node-')
+    local IS_LINKED="$(yq '.settings.linked_node.enabled // "false"' $QTOOLS_CONFIG_FILE)"
+
+    if [ "$IS_LINKED" == "false" ]; then
+        local CURRENT_VERSION=$(systemctl status $QUIL_SERVICE_NAME@main | grep -oP "\-([0-9]+\.)+([0-9]+)\-" | head -n 1 | tr -d 'node-')
+    else
+        local CORE_INDEX_START=$(yq '.settings.linked_node.start_cpu_index' $QTOOLS_CONFIG_FILE)
+        local CURRENT_VERSION=$(systemctl status $QUIL_SERVICE_NAME@$CORE_INDEX_START | grep -oP "\-([0-9]+\.)+([0-9]+)\-" | head -n 1 | tr -d 'node-')
+    fi
     set_current_version $CURRENT_VERSION
     echo $CURRENT_VERSION
 }
