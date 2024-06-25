eecd6d5713acd6354994cf11bff80a674763fcfc
Base Dev <gitlab.dollop533@passmail.net>
2024-06-25 12:30:29 +0300

    update for services

--- scripts/grpc/get-node-version.sh
+++ scripts/grpc/get-node-version.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-cat $QUIL_NODE_PATH/config/version.go | grep -A 1 'func GetVersion() \[\]byte {' | grep -Eo '0x[0-9a-fA-F]+' | xargs printf '%d.%d.%d'
+IS_LINKED="$(yq '.settings.linked_node.enabled' $QTOOLS_CONFIG_FILE)"
+
+if [ "$IS_LINKED" == "false" ]; then
+    local CURRENT_VERSION=$(systemctl status $QUIL_SERVICE_NAME@main | grep -oP "\-([0-9]+\.)+([0-9]+)\-" | head -n 1 | tr -d 'node-')
+else
+    local CORE_INDEX_START=$(yq '.settings.linked_node.start_cpu_index' $QTOOLS_CONFIG_FILE)
+    local CURRENT_VERSION=$(systemctl status $QUIL_SERVICE_NAME@$CORE_INDEX_START | grep -oP "\-([0-9]+\.)+([0-9]+)\-" | head -n 1 | tr -d 'node-')
+fi
+set_current_version $CURRENT_VERSION
+echo $CURRENT_VERSION
+echo ""
