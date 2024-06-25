61b641b4194249d5fee17615341deaf0820672b9
Base Dev <gitlab.dollop533@passmail.net>
2024-06-25 12:28:43 +0300

    update for main

--- scripts/diagnostics/view-log.sh
+++ scripts/diagnostics/view-log.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-sudo journalctl -u $QUIL_SERVICE_NAME -f --no-hostname -o cat
+if [ "$IS_LINKED" != "true" ]; then
+    # if is linked, then start the secondary processes
+    # otherwise just start the main process
+    sudo journalctl -u $QUIL_SERVICE_NAME@main -f --no-hostname -o cat
+else
+    CORE_INDEX_START=$(yq '.settings.linked_node.start_cpu_index' $QTOOLS_CONFIG_FILE)
+    CORE_INDEX_STOP=$(yq '.settings.linked_node.end_cpu_index' $QTOOLS_CONFIG_FILE)
+
+    for ((i = $CORE_INDEX_START ; i <= $CORE_INDEX_STOP ; i++)); do
+        sudo journalctl -u $QUIL_SERVICE_NAME@$i -f --no-hostname -o cat
+    done
+fi
