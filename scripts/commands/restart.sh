3a538b50bfefd54a801dc282d432129247a0ac71
Base Dev <gitlab.dollop533@passmail.net>
2024-07-01 19:56:33 -0800

    change restart

--- scripts/commands/restart.sh
+++ scripts/commands/restart.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-if [ "$IS_LINKED" == "true" ]; then
-    # if is linked, then start the secondary processes
-    CORE_INDEX_START=$(yq '.settings.linked_node.start_cpu_index' $QTOOLS_CONFIG_FILE)
-    CORE_INDEX_STOP=$(yq '.settings.linked_node.end_cpu_index' $QTOOLS_CONFIG_FILE)
-    NODE_PROCESS_START=$(yq '.settings.linked_node.process_start_index' $QTOOLS_CONFIG_FILE)
-
-    for ((i = $CORE_INDEX_START ; i <= $CORE_INDEX_STOP ; i++)); do
-        NODE_ARGS="$NODE_ARGS --core=$(expr $i)" 
-        systemctl restart $QUIL_SERVICE_NAME@$i.service
-    done
-else
-    # otherwise just start the main process
-    systemctl restart $QUIL_SERVICE_NAME@main.service
-fi
+qtools stop
+qtools start
