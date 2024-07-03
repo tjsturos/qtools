513749a28a76d09792dcef364d768dfcfe156107
Base Dev <gitlab.dollop533@passmail.net>
2024-07-02 22:53:06 -0800

    add logging for qtool tool shortcut

--- scripts/install/create-qtools-symlink.sh
+++ scripts/install/create-qtools-symlink.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 if [ -L "$QTOOLS_BIN_PATH" ]; then
+    log "Removing existing qtools shortcut"
     sudo rm $QTOOLS_BIN_PATH
 fi
 
 sudo ln -s $QTOOLS_PATH/qtools.sh $QTOOLS_BIN_PATH
+
+if [ -L "$QTOOLS_BIN_PATH" ]; then
+    log "Successfully added QTools shortcut"
+else 
+    log "Did not add QTools shortcut"
+fi
