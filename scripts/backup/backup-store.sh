a2819515b97223913f51c53fcd6410931850aefc
Base Dev <gitlab.dollop533@passmail.net>
2024-07-03 00:43:05 -0800

    delete old files from backups

--- scripts/backup/backup-store.sh
+++ scripts/backup/backup-store.sh
@@ -15,7 +15,7 @@ if [ "$IS_BACKUP_ENABLED" == 'true' ]; then
   fi
 
   ssh $SSH_ALIAS "mkdir -p $REMOTE_DIR"
-  rsync -avz --ignore-existing -e ssh "$QUIL_NODE_PATH/.config" "$SSH_ALIAS:$REMOTE_DIR"
+  rsync -avz --delete -e ssh "$QUIL_NODE_PATH/.config" "$SSH_ALIAS:$REMOTE_DIR"
 else
   log "Backup for $LOCAL_HOSTNAME is not enabled. Modify the qtools config to enable."
 fi
