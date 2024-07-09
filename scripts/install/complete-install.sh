ad2c41262f3cf03f3e250d686a1784707d95e38f
Base Dev <gitlab.dollop533@passmail.net>
2024-07-09 00:08:08 -0800

    allow to restore from backup on install

--- scripts/install/complete-install.sh
+++ scripts/install/complete-install.sh
@@ -18,13 +18,19 @@ qtools setup-firewall
 qtools install-cron
 
 if [ "$IS_LINKED" != "true" ]; then
-    # This first command generates a default config file
-    BINARY_NAME="$(get_versioned_node)"
-    BINARY_FILE=$QUIL_NODE_PATH/$BINARY_NAME
-    $BINARY_FILE -peer-id
-    sleep 3
-    if [ -f $BINARY_FILE ]; then 
-        qtools modify-config
+    RESTORE_ON_INSTALL="$(yq '.settings.backups.restore_on_install' $QTOOLS_CONFIG_FILE)"
+    if [ "$RESTORE_ON_INSTALL" == "true" ]; then
+        log "Attempting to restore from remote backup. Note: backups must be enabled and configured properly (and connected to at least once) for this to work."
+        qtools restore-backup
+    else
+        # This first command generates a default config file
+        BINARY_NAME="$(get_versioned_node)"
+        BINARY_FILE=$QUIL_NODE_PATH/$BINARY_NAME
+        $BINARY_FILE -peer-id
+        sleep 3
+        if [ -f $BINARY_FILE ]; then 
+            qtools modify-config
+        fi
     fi
 fi
 
