b6f32cc75c2e3e535894aaad3acc960a9b80a9c8
Base Dev <gitlab.dollop533@passmail.net>
2024-07-12 23:13:55 -0800

    include binaries with project

--- scripts/install/install-yq.sh
+++ scripts/install/install-yq.sh
@@ -2,8 +2,15 @@
 # HELP: Installs \'yq\' version 4.20.1 for parsing config files.
 VERSION=$(qyaml '.settings.install.yq.version' $QTOOLS_CONFIG_FILE)
 BINARY=$(qyaml '.settings.install.yq.binary' $QTOOLS_CONFIG_FILE)
-COMPRESSED_FILENAME=${BINARY}.tar.gz
-sudo wget -q https://github.com/mikefarah/yq/releases/download/${VERSION}/${COMPRESSED_FILENAME} 
+cd $QTOOLS_PATH/binaries
+
+COMPRESSED_FILENAME=${BINARY}_${VERSION}.tar.gz
+
+if [ ! -f $COMPRESSED_FILENAME ]; then
+    sudo wget -q https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz 
+    mv ${BINARY}.tar.gz $COMPRESSED_FILENAME
+fi
+
 tar -xzf $COMPRESSED_FILENAME &> /dev/null
 
 BINARY_INSTALL_DIR=$(qyaml '.settings.install.yq.binary_install_dir' $QTOOLS_CONFIG_FILE)
@@ -13,4 +20,3 @@ if [ -f $BINARY_INSTALL_DIR/yq ]; then
 fi
 
 sudo mv $BINARY $BINARY_INSTALL_DIR/yq
-sudo rm $COMPRESSED_FILENAME
