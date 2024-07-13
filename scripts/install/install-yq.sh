665422f28f6eb9179a787374ccb1eb1afb8d9b94
Base Dev <gitlab.dollop533@passmail.net>
2024-07-12 22:53:04 -0800

    remote parens

--- scripts/install/install-yq.sh
+++ scripts/install/install-yq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # HELP: Installs \'yq\' version 4.20.1 for parsing config files.
-VERSION="$(qyaml '.settings.install.yq.version' $QTOOLS_CONFIG_FILE)"
-BINARY="$(qyaml '.settings.install.yq.binary' $QTOOLS_CONFIG_FILE)"
+VERSION=$(qyaml '.settings.install.yq.version' $QTOOLS_CONFIG_FILE)
+BINARY=$(qyaml '.settings.install.yq.binary' $QTOOLS_CONFIG_FILE)
 COMPRESSED_FILENAME=${BINARY}.tar.gz
 sudo wget -q https://github.com/mikefarah/yq/releases/download/${VERSION}/${COMPRESSED_FILENAME} 
 tar -xzf $COMPRESSED_FILENAME &> /dev/null
