637357d118952796450a8fee5bfb02d53d145f58
Base Dev <gitlab.dollop533@passmail.net>
2024-07-13 23:20:09 -0800

    stop status command

--- scripts/grpc/node-version.sh
+++ scripts/grpc/node-version.sh
@@ -4,11 +4,10 @@
 IS_LINKED="$(yq '.settings.linked_node.enabled' $QTOOLS_CONFIG_FILE)"
 
 if [ "$IS_LINKED" == "false" ]; then
-    CURRENT_VERSION=$(systemctl status $QUIL_SERVICE_NAME@main | grep -oP "\-([0-9]+\.)+([0-9]+)\-" | head -n 1 | tr -d 'node-')
+    CURRENT_VERSION=$(sudo systemctl status $QUIL_SERVICE_NAME@main --no-pager | grep -oP "\-([0-9]+\.)+([0-9]+)\-" | head -n 1 | tr -d 'node-')
 else
     CORE_INDEX_START=$(yq '.settings.linked_node.start_cpu_index' $QTOOLS_CONFIG_FILE)
-    CURRENT_VERSION=$(systemctl status $QUIL_SERVICE_NAME@$CORE_INDEX_START | grep -oP "\-([0-9]+\.)+([0-9]+)\-" | head -n 1 | tr -d 'node-')
+    CURRENT_VERSION=$(sudo systemctl status $QUIL_SERVICE_NAME@$CORE_INDEX_START --no-pager | grep -oP "\-([0-9]+\.)+([0-9]+)\-" | head -n 1 | tr -d 'node-')
 fi
 set_current_version $CURRENT_VERSION
 echo $CURRENT_VERSION
-echo ""
