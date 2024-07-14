e0949cc5b93fb6fc3d1c937547f381cd55cab7ae
Base Dev <gitlab.dollop533@passmail.net>
2024-07-13 23:04:21 -0800

    remove ubuntu pro messaging

--- scripts/install/customization.sh
+++ scripts/install/customization.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 # IGNORE
-
+# remove ubuntu pro messages
+sudo mv /etc/apt/apt.conf.d/20apt-esm-hook.conf /etc/apt/apt.conf.d/20apt-esm-hook.conf.bak
 # make sure bandwidth is optimized 
 FILE_SYSCTL=/etc/sysctl.conf
 append_to_file $FILE_SYSCTL "net.core.rmem_max = 600000000" false
