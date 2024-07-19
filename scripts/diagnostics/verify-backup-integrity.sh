4fb040d3be50ebe261b01dd24f7994994ee7929f
Base Dev <gitlab.dollop533@passmail.net>
2024-07-19 11:28:16 -0800

    add checks

--- scripts/diagnostics/verify-backup-integrity.sh
+++ scripts/diagnostics/verify-backup-integrity.sh
@@ -9,15 +9,25 @@ if [ "$IS_BACKUP_ENABLED" == 'true' ]; then
     fi
     LOCAL_PATH="$QUIL_PATH/app/.config"
     REMOTE_PATH="~/backups/$NODE_BACKUP_DIR/.config"
-    SSH_ALIAS="$(yq '.settings.backups.ssh_alias' $QTOOLS_CONFIG_FILE)"
     REMOTE_URL="$(yq '.settings.backups.backup_url' $QTOOLS_CONFIG_FILE)"
     REMOTE_USER="$(yq '.settings.backups.remote_user' $QTOOLS_CONFIG_FILE)"
     SSH_KEY_PATH="$(yq '.settings.backups.ssh_key_path' $QTOOLS_CONFIG_FILE)"
 
+    # Check if any required variable is empty
+    if [ "$REMOTE_DIR" == "/$NODE_BACKUP_DIR/" ] || [ -z "$REMOTE_URL" ] || [ -z "$REMOTE_USER" ] || [ -z "$SSH_KEY_PATH" ]; then
+        echo "One or more required backup settings are missing in the configuration."
+        exit 1
+    fi
+    
+    if [ ! -f $SSH_KEY_PATH ]; then
+        echo "SSH key $SSH_KEY_PATH does not exist"
+        exit 1
+    fi
+
     ssh -i $SSH_KEY_PATH -q -o BatchMode=yes -o ConnectTimeout=5 $REMOTE_USER@$REMOTE_URL exit
 
     if [ $? -ne 0 ]; then
-        echo "SSH alias $SSH_ALIAS does not exist or is not reachable."
+        echo "SSH alias $REMOTE_USER@$REMOTE_URL or $SSH_KEY_PATH does not exist or is not reachable."
         exit 1
     fi
 
