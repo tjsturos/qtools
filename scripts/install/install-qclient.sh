2fb5768f54abd7f499c60d668391b31464a03d72
Base Dev <gitlab.dollop533@passmail.net>
2024-08-30 01:08:55 -0800

    make directory

--- scripts/install/install-qclient.sh
+++ scripts/install/install-qclient.sh
@@ -8,6 +8,9 @@ if [[ ! -f "$QUIL_CLIENT_PATH/$CURRENT_QCLIENT_BINARY" ]]; then
     
     get_remote_quil_files qclient_files[@] $QUIL_CLIENT_PATH
 
+    # Ensure the directory exists
+    mkdir -p $(dirname $QUIL_QCLIENT_BIN)
+
     if [ -f $QUIL_CLIENT_PATH/$CURRENT_QCLIENT_BINARY ]; then
         sudo chmod +x $QUIL_CLIENT_PATH/$CURRENT_QCLIENT_BINARY
 
