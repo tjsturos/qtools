4442e44f2f1eb9e14aca055890c753ab458ffd62
Base Dev <gitlab.dollop533@passmail.net>
2024-08-30 01:10:44 -0800

    make sure the right directory exists

--- scripts/install/install-qclient.sh
+++ scripts/install/install-qclient.sh
@@ -3,13 +3,13 @@
 
 CURRENT_QCLIENT_BINARY="$(get_versioned_qclient)"
 
+# Ensure the directory exists
+mkdir -p $(dirname $QUIL_QCLIENT_PATH)
+
 if [[ ! -f "$QUIL_CLIENT_PATH/$CURRENT_QCLIENT_BINARY" ]]; then
     qclient_files=$(fetch_available_files "https://releases.quilibrium.com/qclient-release")
     
     get_remote_quil_files qclient_files[@] $QUIL_CLIENT_PATH
-
-    # Ensure the directory exists
-    mkdir -p $(dirname $QUIL_QCLIENT_BIN)
 
     if [ -f $QUIL_CLIENT_PATH/$CURRENT_QCLIENT_BINARY ]; then
         sudo chmod +x $QUIL_CLIENT_PATH/$CURRENT_QCLIENT_BINARY
