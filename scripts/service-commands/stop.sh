675a80907328fa2b4c97d10f8c46780253a0e0e9
Base Dev <gitlab.dollop533@passmail.net>
2024-08-30 01:52:14 -0800

    update stop for macos

--- scripts/service-commands/stop.sh
+++ scripts/service-commands/stop.sh
@@ -3,7 +3,13 @@
 # Usage: qtools stop
 
 # otherwise just start the main process
-sudo systemctl stop $QUIL_SERVICE_NAME@main.service
-wait
-# and to make sure any stray node commands are exited
-pgrep -f node | grep -v $$ | xargs -r sudo kill -9
+# Stop the service using launchctl
+launchctl unload "$QUIL_SERVICE_FILE"
+
+# Check if the service has stopped
+if ! launchctl list | grep -q "$QUIL_SERVICE_NAME"; then
+    log "Node service stopped successfully."
+else
+    log "Failed to stop node service. Attempting to force quit..."
+    launchctl remove "$QUIL_SERVICE_NAME"
+fi
