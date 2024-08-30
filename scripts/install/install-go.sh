dc3400a5fcef6100791c4b497f0add61f0499f50
Base Dev <gitlab.dollop533@passmail.net>
2024-08-30 10:10:58 -0800

    update go install

--- scripts/install/install-go.sh
+++ scripts/install/install-go.sh
@@ -3,23 +3,8 @@
 log "Installing Go"
 
 GO_VERSION=$(yq '.settings.install.go.version' $QTOOLS_CONFIG_FILE)
-OS_ARCH="$(get_os_arch)"
-GO_COMPRESSED_FILE="go${GO_VERSION}.${OS_ARCH}.tar.gz"
-GO_DOWNLOAD_URL="https://go.dev/dl/${GO_COMPRESSED_FILE}"
 
-log "Downloading $GO_COMPRESSED_FILE..."
-curl -O $GO_DOWNLOAD_URL
-
-log "Uncompressing $GO_COMPRESSED_FILE"
-sudo tar -C /usr/local -xzf $GO_COMPRESSED_FILE
-
-rm $GO_COMPRESSED_FILE
-
-append_to_file $BASHRC_FILE "export GOROOT=$GOROOT" false
-append_to_file $BASHRC_FILE "export GOPATH=$GOPATH" false
-append_to_file $BASHRC_FILE "export PATH=\$GOPATH/bin:\$GOROOT/bin:\$PATH" false
-
-source $BASHRC_FILE
+brew install go@${GO_VERSION}
 
 if command_exists go; then
     log "Go installed successfully. Version: $(go version)"
