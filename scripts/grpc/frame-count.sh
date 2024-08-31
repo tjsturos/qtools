d6e921c709c5b67f3af1ddf158a89ae304f9fb0c
Base Dev <gitlab.dollop533@passmail.net>
2024-08-30 22:00:53 -0800

    modify grep for macos

--- scripts/grpc/frame-count.sh
+++ scripts/grpc/frame-count.sh
@@ -4,7 +4,7 @@
 IS_APP_FINISHED_STARTING="$(is_app_finished_starting)"
 
 if [ $IS_APP_FINISHED_STARTING == "true" ]; then
-    echo "$(grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNodeInfo | grep -oP '"maxFrame":\s*"\K[^"]+')"
+    echo "$(grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNodeInfo | grep -E '"maxFrame":[[:space:]]*"' | sed -E 's/.*"maxFrame":[[:space:]]*"([^"]+)".*/\1/')"
 else
     echo "Could not fetch frame count.  App hasn't finished starting."
 fi
--- scripts/grpc/node-version.sh
+++ scripts/grpc/node-version.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-# HELP: Prints this node\'s version that is running in the service.
+# HELP: Prints this node's version that is running in the service.
 
-CURRENT_VERSION=$(launchctl list | grep $QUIL_SERVICE_NAME | awk '{print $3}' | xargs sudo plutil -p | grep CFBundleVersion | awk -F'"' '{print $4}')
-set_current_version $CURRENT_VERSION
-echo $CURRENT_VERSION
+# For macOS, we use launchctl to get the service information
+CURRENT_VERSION=$(launchctl list | grep "$QUIL_SERVICE_NAME" | awk '{print $3}' | xargs plutil -p | grep CFBundleVersion | awk -F'"' '{print $4}')
+
+# Set the current version in the configuration file
+set_current_version "$CURRENT_VERSION"
+
+# Print the current version
+echo "$CURRENT_VERSION"
