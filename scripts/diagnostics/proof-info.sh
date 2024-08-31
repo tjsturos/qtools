14b766afdb054891d8b65b95640c3c7956403722
Base Dev <gitlab.dollop533@passmail.net>
2024-08-31 14:05:51 -0800

    update proof info for log file change

--- scripts/diagnostics/proof-info.sh
+++ scripts/diagnostics/proof-info.sh
@@ -2,8 +2,7 @@
 
 fetch_proof_info() {
     local UPTIME="$(get_last_started_at)"
-    local LOG_FILE=$(launchctl list | grep "$QUIL_SERVICE_NAME" | awk '{print $3}' | xargs launchctl list -x | grep StandardOutPath | awk -F '<string>' '{print $2}' | awk -F '</string>' '{print $1}')
-    local LOG_OUTPUT=$(sed -n "/$UPTIME/,\$p" "$LOG_FILE")
+    local LOG_OUTPUT=$(sed -n "/$UPTIME/,\$p" "$QUIL_LOG_FILE")
     
     local LAST_STORING_PROOF=$(echo "$LOG_OUTPUT" | grep "storing proof" | tail -n 1)
     local LAST_COMPLETED_PROOF=$(echo "$LOG_OUTPUT" | grep "completed duration proof" | tail -n 1)
