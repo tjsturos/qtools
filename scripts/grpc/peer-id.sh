be4224482efbe9736c9887d97082831fc3531c20
Base Dev <gitlab.dollop533@passmail.net>
2024-08-31 16:39:25 -0800

    update get peer id

--- scripts/grpc/peer-id.sh
+++ scripts/grpc/peer-id.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # HELP: Gets this node's Peer ID. Attempts to use grpcurl to get the peer id, but in the event of failure, will use the node binary's -peer-id command.
 
-PEER_ID="$(grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNodeInfo 2> /dev/null | grep -E '"peerId":\s*"[^"]+"' | sed -E 's/.*"peerId":\s*"([^"]+)".*/\1/')"
+PEER_ID="$(grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNodeInfo 2> /dev/null | grep 'peerId' | cut -d':' -f2 | sed -E 's/.*\"(([^)]+)\".*/\1/')"
 
 # Check if a Peer ID was found
 if [ -z "$PEER_ID" ]; then
-    # Run the node-get-peer-id script with the extracted Peer ID
-    cd $QUIL_NODE_PATH
+    # Use the node binary's -peer-id command
+    cd "$QUIL_NODE_PATH"
 
     QUIL_BIN="$(get_versioned_node)"
 
@@ -14,11 +14,9 @@ if [ -z "$PEER_ID" ]; then
 
     PEER_ID="$(echo "$OUTPUT" | sed -n 's/Peer ID: //p')"
 
-    if [ -n "$PEER_ID" ]; then
-        echo "$PEER_ID"
-    else
-        log "Could not find Peer ID."
+    if [ -z "$PEER_ID" ]; then
+        exit 1
     fi
-else
-    echo "$PEER_ID"
 fi
+
+echo "$PEER_ID"
