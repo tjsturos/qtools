5aa71a1eb71908c54790b8a754f93062fe8159e9
Base Dev <gitlab.dollop533@passmail.net>
2024-08-31 16:40:28 -0800

    fix syntax error

--- scripts/grpc/peer-id.sh
+++ scripts/grpc/peer-id.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # HELP: Gets this node's Peer ID. Attempts to use grpcurl to get the peer id, but in the event of failure, will use the node binary's -peer-id command.
 
-PEER_ID="$(grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNodeInfo 2> /dev/null | grep 'peerId' | cut -d':' -f2 | sed -E 's/.*\"(([^)]+)\".*/\1/')"
+PEER_ID="$(grpcurl -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNodeInfo 2> /dev/null | grep 'peerId' | cut -d':' -f2 | sed -E 's/.*\"([^)]+)\".*/\1/')"
 
 # Check if a Peer ID was found
 if [ -z "$PEER_ID" ]; then
