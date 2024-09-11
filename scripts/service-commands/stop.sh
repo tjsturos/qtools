053e7cb4f87a95a2a9589fe5e1908d0de48627bf
Tyler Sturos <tyler.john@qcommander.sh>
2024-09-10 19:37:13 -0800

    on stop backup store and then disable backups

--- scripts/service-commands/stop.sh
+++ scripts/service-commands/stop.sh
@@ -4,6 +4,14 @@
 
 # otherwise just start the main process
 sudo systemctl stop $QUIL_SERVICE_NAME.service
+
+qtools backup-store
+
+# Disable backups after stopping the service
+yq -i '.settings.backups.enabled = false' $QTOOLS_CONFIG_FILE
+
+log "Backups have been disabled in the qtools configuration."
+
 wait
 # and to make sure any stray node commands are exited
 pgrep -f node | grep -v $$ | xargs -r sudo kill -9
