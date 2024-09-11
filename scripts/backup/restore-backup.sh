fa8db5d8dc16f9e565a0bd95a197520345e06ce7
Tyler Sturos <tyler.john@qcommander.sh>
2024-09-10 21:07:33 -0800

    change to peer-id

--- scripts/backup/restore-backup.sh
+++ scripts/backup/restore-backup.sh
@@ -2,14 +2,14 @@
 # HELP: Will restore a backup (if enabled) based on the qtools config settings, otherwise will look for a backup directory with this node\'s hostname.
 # PARAM: --hostname <string>: the hostname to use when restoring the config directory.
 # Usage: qtools restore-backup # Will default to this machine\'s hostname if not defined in the qtools/config.yml
-# Usage: qtools restore-backup --hostname quil-miner-101 # Will look for a ~/backups/quil-miner-101 directory on the backup server.
+# Usage: qtools restore-backup --peer-id Qmabcdefg... # Will look for a ~/backups/Qmabcdefg directory on the backup server.
 
 IS_BACKUP_ENABLED="$(yq '.settings.backups.enabled' $QTOOLS_CONFIG_FILE)"
 LOCAL_HOSTNAME=$(hostname)
 
 if [ "$IS_BACKUP_ENABLED" == 'true' ]; then
   NODE_BACKUP_DIR="$(yq '.settings.backups.node_backup_dir' $QTOOLS_CONFIG_FILE)"
-  if [ "$1" == "--hostname" ]; then
+  if [ "$1" == "--peer-id" ]; then
     shift
     NODE_BACKUP_DIR="$1"
   else
