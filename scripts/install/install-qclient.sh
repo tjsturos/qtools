9712693e6e2dfa6699436c429c4aa388baa810da
Tyler Sturos <tyler.john@qcommander.sh>
2024-09-10 20:53:53 -0800

    make sure qclient path exists

--- scripts/install/install-qclient.sh
+++ scripts/install/install-qclient.sh
@@ -6,6 +6,7 @@ CURRENT_QCLIENT_BINARY="$(get_versioned_qclient)"
 if [ ! -f "$QUIL_CLIENT_PATH/$CURRENT_QCLIENT_BINARY" ]; then
     qclient_files=$(fetch_available_files "https://releases.quilibrium.com/qclient-release")
     
+    mkdir -p $QUIL_CLIENT_PATH
     get_remote_quil_files qclient_files[@] $QUIL_CLIENT_PATH
 
     if [ -f $QUIL_CLIENT_PATH/$CURRENT_QCLIENT_BINARY ]; then
