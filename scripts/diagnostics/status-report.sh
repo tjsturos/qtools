678bda0d354dadf66ff765a9bef4711b50f83c41
Tyler Sturos <tyler.john@qcommander.sh>
2024-09-16 21:06:18 -0800

    print optionally in json

--- scripts/diagnostics/status-report.sh
+++ scripts/diagnostics/status-report.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# Initialize variables
+JSON_OUTPUT=false
+REPORT_DATA=()
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --json)
+            JSON_OUTPUT=true
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
 
 # Define colored icons
 GREEN_CHECK="\e[32m✔\e[0m"
@@ -11,20 +27,33 @@ NC="\e[0m"
 check_ports_status() {
     local PORTS_ACTUAL_OUTPUT="$(qtools ports-listening)"
     local ALL_PORTS_FUNCTIONAL=true
+    local port_status=()
     while IFS= read -r LINE; do
         if [[ $LINE == *"was not found listening"* ]]; then
             PORT=$(echo $LINE | grep -oP '(?<=Port )\d+')
-            echo -e "${RED_CROSS} Port $PORT is not working as expected"
+            port_status+=("Port $PORT is not working as expected")
             ALL_PORTS_FUNCTIONAL=false
         elif [[ $LINE == *"still starting up"* ]]; then
             PORT=$(echo $LINE | grep -oP '(?<=Port )\d+')
-            echo -e "${WARNING} $LINE"
+            port_status+=("$LINE")
             ALL_PORTS_FUNCTIONAL=false
         fi
     done <<< "$PORTS_ACTUAL_OUTPUT"
 
     if $ALL_PORTS_FUNCTIONAL; then
-        echo -e "${GREEN_CHECK} All ports functional"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("ports_status:All ports functional")
+        else
+            echo -e "${GREEN_CHECK} All ports functional"
+        fi
+    else
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("ports_status:${port_status[*]}")
+        else
+            for status in "${port_status[@]}"; do
+                echo -e "${RED_CROSS} $status"
+            done
+        fi
     fi
 }
 
@@ -32,9 +61,17 @@ check_hourly_reward_rate() {
     local HOURLY_REWARD_RATE=$(qtools hourly-reward-rate 5)
 
     if [[ $HOURLY_REWARD_RATE =~ ^-?[0-9]+([.][0-9]+)?$ ]]; then
-        echo -e "${GREEN_CHECK} Hourly reward rate: $HOURLY_REWARD_RATE"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("hourly_reward_rate:$HOURLY_REWARD_RATE")
+        else
+            echo -e "${GREEN_CHECK} Hourly reward rate: $HOURLY_REWARD_RATE"
+        fi
     else
-        echo -e "${RED_CROSS} Invalid hourly reward rate: $HOURLY_REWARD_RATE"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("hourly_reward_rate:Invalid hourly reward rate: $HOURLY_REWARD_RATE")
+        else
+            echo -e "${RED_CROSS} Invalid hourly reward rate: $HOURLY_REWARD_RATE"
+        fi
     fi
 }
 
@@ -42,17 +79,50 @@ check_service_status() {
     local output=$(qtools status)
 
     if echo "$output" | grep -q "Active: active (running)"; then
-        echo -e "${GREEN_CHECK} Node service is running (active)"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("node_service_status:Node service is running (active)")
+        else
+            echo -e "${GREEN_CHECK} Node service is running (active)"
+        fi
         local version=$(qtools node-version)
         local uptime=$(echo "$output" | grep -oP '(?<=since ).*')
-        echo -e "${GREEN_CHECK} Node Version: $version"
-        echo -e "${GREEN_CHECK} Node Uptime: $uptime"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("node_version:$version")
+            REPORT_DATA+=("node_uptime:$uptime")
+        else
+            echo -e "${GREEN_CHECK} Node Version: $version"
+            echo -e "${GREEN_CHECK} Node Uptime: $uptime"
+        fi
     elif echo "$output" | grep -q "Active: inactive (dead)"; then
-        echo -e "${RED_CROSS} Node service is not running (dead)"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("node_service_status:Node service is not running (dead)")
+        else
+            echo -e "${RED_CROSS} Node service is not running (dead)"
+        fi
     elif echo "$output" | grep -q "Active: activating (auto-restart)"; then
-        echo -e "${WARNING} Node is trying to restart (activating with auto-restart)"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("node_service_status:Node is trying to restart (activating with auto-restart)")
+        else
+            echo -e "${WARNING} Node is trying to restart (activating with auto-restart)"
+        fi
     else 
-        echo -e "${RED_CROSS} Unable to determine the status of the node"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("node_service_status:Unable to determine the status of the node")
+        else
+            echo -e "${RED_CROSS} Unable to determine the status of the node"
+        fi
+    fi
+
+    # Check max_threads setting
+    local max_threads=$(yq '.service.max_threads' $QTOOLS_CONFIG_FILE)
+    local total_threads=$(nproc)
+    
+    if [[ $max_threads != "null" && $max_threads != "false" ]]; then
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("max_threads:Max threads is set to $max_threads/$total_threads.")
+        else
+            echo -e "${INFO_ICON} Max threads is set to $max_threads/$total_threads."
+        fi
     fi
 }
 
@@ -66,9 +136,17 @@ check_command_installed() {
             version=$($version_command)
             message+=" (version: $version)"
         fi
-        echo -e "$message"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("${command_name}_installed:$message")
+        else
+            echo -e "$message"
+        fi
     else
-        echo -e "${RED_CROSS} $command_name is not installed"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("${command_name}_installed:${RED_CROSS} $command_name is not installed")
+        else
+            echo -e "${RED_CROSS} $command_name is not installed"
+        fi
     fi
 }
 
@@ -76,12 +154,24 @@ check_unclaimed_balance() {
     local BALANCE=$(qtools unclaimed-balance)
 
     if [[ $BALANCE =~ ^-?[0-9]+([.][0-9]+)?$ ]]; then
-        echo -e "${GREEN_CHECK} Unclaimed balance: $BALANCE"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("unclaimed_balance:$BALANCE")
+        else
+            echo -e "${GREEN_CHECK} Unclaimed balance: $BALANCE"
+        fi
     else
         if [ ! -z "$(echo $BALANCE | grep 'finished starting')" ]; then
-            echo -e "${WARNING} App hasn't finished starting up-- can't fetch balance right now."
+            if $JSON_OUTPUT; then
+                REPORT_DATA+=("unclaimed_balance:App hasn't finished starting up-- can't fetch balance right now.")
+            else
+                echo -e "${WARNING} App hasn't finished starting up-- can't fetch balance right now."
+            fi
         else
-            echo -e "${RED_CROSS} Invalid unclaimed balance: $BALANCE"
+            if $JSON_OUTPUT; then
+                REPORT_DATA+=("unclaimed_balance:Invalid unclaimed balance: $BALANCE")
+            else
+                echo -e "${RED_CROSS} Invalid unclaimed balance: $BALANCE"
+            fi
         fi
     fi
 }
@@ -90,9 +180,17 @@ check_peer_id() {
     local PEER_ID=$(qtools peer-id)
 
     if [[ -n $PEER_ID ]]; then
-        echo -e "${GREEN_CHECK} Peer ID: $PEER_ID"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("peer_id:$PEER_ID")
+        else
+            echo -e "${GREEN_CHECK} Peer ID: $PEER_ID"
+        fi
     else
-        echo -e "${RED_CROSS} Unable to retrieve Peer ID"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("peer_id:Unable to retrieve Peer ID")
+        else
+            echo -e "${RED_CROSS} Unable to retrieve Peer ID"
+        fi
     fi
 }
 
@@ -101,12 +199,24 @@ check_frame_count() {
     
     if [[ -n $FRAME_COUNT ]]; then
         if [ ! -z "$(echo $FRAME_COUNT | grep 'finished starting')" ]; then
-            echo -e "${WARNING} App hasn't finished starting up-- can't fetch frames right now."
+            if $JSON_OUTPUT; then
+                REPORT_DATA+=("frame_count:App hasn't finished starting up-- can't fetch frames right now.")
+            else
+                echo -e "${WARNING} App hasn't finished starting up-- can't fetch frames right now."
+            fi
         else
-            echo -e "${GREEN_CHECK} Frame Count: $FRAME_COUNT"
+            if $JSON_OUTPUT; then
+                REPORT_DATA+=("frame_count:$FRAME_COUNT")
+            else
+                echo -e "${GREEN_CHECK} Frame Count: $FRAME_COUNT"
+            fi
         fi
     else
-        echo -e "${RED_CROSS} Unable to retrieve frame count"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("frame_count:Unable to retrieve frame count")
+        else
+            echo -e "${RED_CROSS} Unable to retrieve frame count"
+        fi
     fi
 }
 
@@ -116,20 +226,36 @@ check_backup_status() {
 
     if [[ "$enabled" == "true" ]]; then
         local node_backup_dir=$(yq '.settings.backups.node_backup_dir' "$config_file" 2>/dev/null || qyaml '.settings.backups.node_backup_dir' "$config_file")
+        if [ -z "$node_backup_dir" ]; then
+            node_backup_dir=$(qtools peer-id)
+        fi
         local backup_url=$(yq '.settings.backups.backup_url' "$config_file" 2>/dev/null || qyaml '.settings.backups.backup_url' "$config_file")
         local remote_user=$(yq '.settings.backups.remote_user' "$config_file" 2>/dev/null || qyaml '.settings.backups.remote_user' "$config_file")
         local ssh_key_path=$(yq '.settings.backups.ssh_key_path' "$config_file" 2>/dev/null || qyaml '.settings.backups.ssh_key_path' "$config_file")
         local remote_backup_dir=$(yq '.settings.backups.remote_backup_dir' "$config_file" 2>/dev/null || qyaml '.settings.backups.remote_backup_dir' "$config_file")
 
-        echo -e "${GREEN_CHECK} Backup Status:"
-        echo -e "    Enabled: $enabled"
-        echo -e "    Node Backup Directory: $node_backup_dir"
-        echo -e "    Backup URL: $backup_url"
-        echo -e "    Remote User: $remote_user"
-        echo -e "    SSH Key Path: $ssh_key_path"
-        echo -e "    Remote Backup Directory: $remote_backup_dir"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("backup_status:Enabled")
+            REPORT_DATA+=("node_backup_dir:$node_backup_dir")
+            REPORT_DATA+=("backup_url:$backup_url")
+            REPORT_DATA+=("remote_user:$remote_user")
+            REPORT_DATA+=("ssh_key_path:$ssh_key_path")
+            REPORT_DATA+=("remote_backup_dir:$remote_backup_dir")
+        else
+            echo -e "${GREEN_CHECK} Backup Status:"
+            echo -e "    Enabled: $enabled"
+            echo -e "    Node Backup Directory: $node_backup_dir"
+            echo -e "    Backup URL: $backup_url"
+            echo -e "    Remote User: $remote_user"
+            echo -e "    SSH Key Path: $ssh_key_path"
+            echo -e "    Remote Backup Directory: $remote_backup_dir"
+        fi
     else
-        echo -e "${RED_CROSS} Backups are not enabled"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("backup_status:Backups are not enabled")
+        else
+            echo -e "${RED_CROSS} Backups are not enabled"
+        fi
     fi
 }
 
@@ -152,16 +278,75 @@ check_proof_info() {
         proof_icon="🌈"  # Da bomb (best)
     fi
 
-    echo -e "${proof_icon} Last storing proof increment: $storing_increment"
-    echo -e "${proof_icon} Last completed proof increment: $completed_increment"
-    echo -e "${BLUE}${INFO_ICON}${NC} Last proof completion time: $time_taken seconds"
+    if $JSON_OUTPUT; then
+        REPORT_DATA+=("last_storing_proof_increment:$storing_increment")
+        REPORT_DATA+=("last_completed_proof_increment:$completed_increment")
+        REPORT_DATA+=("last_proof_completion_time:$time_taken seconds")
+    else
+        echo -e "${proof_icon} Last storing proof increment: $storing_increment"
+        echo -e "${proof_icon} Last completed proof increment: $completed_increment"
+        echo -e "${BLUE}${INFO_ICON}${NC} Last proof completion time: $time_taken seconds"
+    fi
+}
+
+check_statistics_enabled() {
+    local config_file="$QTOOLS_CONFIG_FILE"
+    local enabled=$(yq '.settings.statistics.enabled' "$config_file" 2>/dev/null || qyaml .settings.statistics.enabled "$config_file")
+
+    if [[ "$enabled" == "true" ]]; then
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("statistics_enabled:true")
+        else
+            echo -e "${GREEN_CHECK} Statistics are enabled"
+        fi
+    else
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("statistics_enabled:false")
+        else
+            echo -e "${RED_CROSS} Statistics are not enabled"
+        fi
+    fi
+}
+
+check_diagnostics_enabled() {
+    local config_file="$QTOOLS_CONFIG_FILE"
+    local enabled=$(yq '.settings.diagnostics.enabled' "$config_file" 2>/dev/null || qyaml .settings.diagnostics.enabled "$config_file")
+
+    if [[ "$enabled" == "true" ]]; then
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("diagnostics_enabled:true")
+        else
+            echo -e "${GREEN_CHECK} Diagnostics are enabled"
+        fi
+    else
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("diagnostics_enabled:false")
+        else
+            echo -e "${RED_CROSS} Diagnostics are not enabled"
+        fi
+    fi
 }
 
 check_hardware_info() {
     local hardware_info=$($QTOOLS_PATH/scripts/diagnostics/hardware-info.sh)
     while IFS='|' read -r key value; do
-        echo -e "${BLUE}${INFO_ICON}${NC} $key: $value"
+        if $JSON_OUTPUT; then
+            REPORT_DATA+=("${key}:${value}")
+        else
+            echo -e "${BLUE}${INFO_ICON}${NC} $key: $value"
+        fi
     done <<< "$hardware_info"
+}
+
+status_report_json() {
+    local json="{"
+    for item in "${REPORT_DATA[@]}"; do
+        IFS=':' read -r key value <<< "$item"
+        json+="\"$key\":\"$value\","
+    done
+    json="${json%,}"  # Remove the trailing comma
+    json+="}"
+    echo "$json"
 }
 
 print_status_report() {
@@ -176,7 +361,13 @@ print_status_report() {
     check_hourly_reward_rate
     check_unclaimed_balance
     check_backup_status
+    check_statistics_enabled
+    check_diagnostics_enabled
     check_hardware_info
+
+    if $JSON_OUTPUT; then
+        status_report_json
+    fi
 }
 
 print_status_report
