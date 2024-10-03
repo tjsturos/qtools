2a124692315b8d6080734a11609ac69a7311b2eb
Tyler Sturos <tyler.john@qcommander.sh>
2024-10-03 12:21:22 -0800

    include install script

--- scripts/shortcuts/install-tailscale.sh
+++ scripts/shortcuts/install-tailscale.sh
@@ -1,6 +1,25 @@
 TAILSCALE_AUTH_KEY="$(yq -r '.settings.install.tailscale.ephemeral_key' $QTOOLS_PATH/config.yml)"
 
+IS_INSTALL="true"
 
+# Loop through command-line parameters
+for param in "$@"; do
+    case $param in
+        --uninstall)
+            IS_INSTALL="false"
+            break
+            ;;
+    esac
+done
+
+if [ "$IS_INSTALL" = "false" ]; then
+    log "Uninstalling Tailscale..."
+    sudo tailscale down
+    sudo apt-get remove tailscale -y
+    exit 0
+fi
+
+log "Installing Tailscale..."
 curl -fsSL https://tailscale.com/install.sh | sh
 
 if [ -z "$TAILSCALE_AUTH_KEY" ]; then
@@ -9,3 +28,4 @@ if [ -z "$TAILSCALE_AUTH_KEY" ]; then
 else
     sudo tailscale up --auth-key $TAILSCALE_AUTH_KEY
 fi
+
