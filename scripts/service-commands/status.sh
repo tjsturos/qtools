77505f0dc7d56d25ab715e33a21dc34f6aa9c617
Tyler Sturos <tyler.john@qcommander.sh>
2024-10-04 13:19:45 -0800

    update status to only check for self in not master

--- scripts/service-commands/status.sh
+++ scripts/service-commands/status.sh
@@ -5,40 +5,34 @@
 IS_CLUSTERING_ENABLED=$(yq '.service.clustering.enabled // false' $QTOOLS_CONFIG_FILE)
 
 if [ "$IS_CLUSTERING_ENABLED" == "true" ]; then
-    # Parse command line arguments
-    # Read the config file
-    config=$(yq eval . $QTOOLS_CONFIG_FILE)
-    
-    # Get the array of servers
-    servers=$(echo "$config" | yq eval '.service.clustering.servers' -)
 
-    # Get the number of servers
-    server_count=$(echo "$servers" | yq eval '. | length' -)
+    if [ "$(is_master)" == "true" ]; then
+        servers=$(get_cluster_ips)
+   
+        # Loop through each server
+        for ip in $servers; do
+            # Check if the IP is the local machine
+            if echo "$(hostname -I)" | grep -q "$ip"; then
+                echo "This is the local machine, continuing..."
+                sudo systemctl status $QUIL_SERVICE_NAME.service --no-pager
+                continue
+            fi
+            
+            # Skip invalid entries
+            if [ -z "$ip" ] || [ "$ip" == "null" ]; then
+                echo "Skipping invalid server entry: $server"
+                continue
+            fi
 
-    # Loop through each server
-    for ((i=0; i<server_count; i++)); do
-        server=$(echo "$servers" | yq eval ".[$i]" -)
-        ip=$(echo "$server" | yq eval '.ip' -)
-
-        # Check if the IP is the local machine
-        if echo "$(hostname -I)" | grep -q "$ip"; then
-            echo "This is the local machine, continuing..."
-            sudo systemctl status $QUIL_SERVICE_NAME.service --no-pager
-            continue
-        fi
-        
-        # Skip invalid entries
-        if [ -z "$ip" ] || [ "$ip" == "null" ]; then
-            echo "Skipping invalid server entry: $server"
-            continue
-        fi
-
-        echo "Checking status of dataworkers on $ip:"
-        
-        # Use ssh to run the command on the remote machine
-        ssh -i ~/.ssh/cluster-key "client@$ip" "sudo systemctl status $QUIL_SERVICE_NAME.service --no-pager"
-        echo "----------------------------------------"
-    done
+            echo "Checking status of dataworkers on $ip:"
+            
+            # Use ssh to run the command on the remote machine
+            ssh -i ~/.ssh/cluster-key "client@$ip" "sudo systemctl status $QUIL_SERVICE_NAME.service --no-pager"
+            echo "----------------------------------------"
+        done
+    else
+        sudo systemctl status $QUIL_SERVICE_NAME.service --no-pager
+    fi
 else
     sudo systemctl status $QUIL_SERVICE_NAME.service --no-pager
 fi
