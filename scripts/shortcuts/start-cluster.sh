3d6559f6ff2ee72db5b8db4e28063739151f828f
Tyler Sturos <tyler.john@qcommander.sh>
2024-10-03 21:53:34 -0800

    refine flow

--- scripts/shortcuts/start-cluster.sh
+++ scripts/shortcuts/start-cluster.sh
@@ -12,50 +12,6 @@ DATA_WORKER_COUNT=$TOTAL_CORES
 INDEX_START=1
 MASTER=false
 
-BINARY=$(get_versioned_node)
-
-# Add this near the top of the script, after other variable declarations
-TOTAL_EXPECTED_DATAWORKERS=0
-
-# Function to create the systemd service file if it doesn't exist
-create_service_file() {
-    local service_file="/etc/systemd/system/$QUIL_SERVICE_NAME-dataworker@.service"
-    if [ "$DRY_RUN" == "true" ]; then
-        echo -e "${BLUE}${INFO_ICON} [DRY RUN] Would create $service_file if it doesn't exist${RESET}"
-    else
-        USER=$(whoami)
-        GROUP=$(id -gn)
-        if [ -z "$USER" ] || [ -z "$GROUP" ]; then
-            echo "Error: Failed to get user or group information"
-            exit 1
-        fi
-        echo -e "${BLUE}${INFO_ICON} Creating $QUIL_SERVICE_NAME-dataworker@.service file...${RESET}"
-        sudo tee "$service_file" > /dev/null <<EOF
-[Unit]
-Description=Quilibrium Dataworker Service
-
-[Service]
-Type=simple
-Restart=always
-RestartSec=50ms
-User=$USER
-Group=$GROUP
-WorkingDirectory=$(yq '.service.working_dir // "'$QUIL_NODE_PATH'"' $QTOOLS_CONFIG_FILE)
-ExecStart=$QUIL_NODE_PATH/$BINARY --core %i
-
-
-[Install]
-WantedBy=multi-user.target
-EOF
-        sudo systemctl daemon-reload
-        echo -e "${BLUE}${INFO_ICON} Service file created and systemd reloaded.${RESET}"
-    fi
-}
-
-# Call the function to create the service file
-create_service_file
-
-
 # Function to display usage information
 usage() {
     echo "Usage: $0 [--data-worker-count <number>] [--index-start <number>] [--master]"
@@ -103,173 +59,47 @@ if ! [[ "$DATA_WORKER_COUNT" =~ ^[1-9][0-9]*$ ]]; then
     exit 1
 fi
 
+# Call the function to create the service file
+create_dataworker_service_file
+
 # Adjust COUNT if master is specified, but only if not all cores are used for workers
 if [ "$MASTER" == "true" ] && [ "$TOTAL_CORES" -eq "$DATA_WORKER_COUNT" ]; then
     DATA_WORKER_COUNT=$((TOTAL_CORES - 1))
 fi
 
-
-
-# Function to start a single core
-start_core() {
-    local CORE=$1
-    if [ "$DRY_RUN" == "true" ]; then
-        echo -e "${BLUE}${INFO_ICON} [DRY RUN] Would enable and start $QUIL_SERVICE_NAME-dataworker@$CORE.service${RESET}"
-    else
-        if ! sudo systemctl start $QUIL_SERVICE_NAME-dataworker@$CORE.service; then
-            echo -e "\e[31mFailed to start $QUIL_SERVICE_NAME-dataworker@$CORE.service.\e[0m"
-        else
-            echo -e "\e[32mStarted $QUIL_SERVICE_NAME-dataworker@$CORE.service\e[0m"
-            sudo systemctl enable $QUIL_SERVICE_NAME-dataworker@$CORE.service
-        fi
-    fi
-}
-
-start_remote_cores() {
-    local IP=$1
-    local CORE_INDEX_START=$2   
-    echo -e "${BLUE}${INFO_ICON} Starting cluster's dataworkers on $IP${RESET}"
-    ssh -i ~/.ssh/cluster-key "$IP" "qtools start-cluster --index-start $CORE_INDEX_START"
-}
-
-start_local_cores() {
-    local CORE_INDEX_START=$1
-    echo -e "${BLUE}${INFO_ICON} Starting cluster's dataworkers on local machine${RESET}"
-    for ((i=0; i<DATA_WORKER_COUNT; i++)); do
-        CORE=$((INDEX_START + i))
-        start_core $CORE &
-    done
-}
-
-
-
-# If master, configure data worker servers
+# Start the master and update the config
 if [ "$MASTER" == "true" ]; then
-    # Read the config file
-   
-    config=$(yq eval . $QTOOLS_CONFIG_FILE)
-    
-    # Get the array of servers
-    servers=$(echo "$config" | yq eval '.service.clustering.servers' -)
 
-    # Clear the existing dataworkerMultiaddrs array
-    yq eval -i '.engine.dataWorkerMultiaddrs = []' "$QUIL_CONFIG_FILE"
+    update_quil_config $DRY_RUN
 
-    # Initialize TOTAL_EXPECTED_DATAWORKERS
-    TOTAL_EXPECTED_DATAWORKERS=0
+    if [ "$DRY_RUN" == "true" ]; then
+        echo -e "${BLUE}${INFO_ICON} [DRY RUN] Starting $QUIL_SERVICE_NAME.service${RESET}"
+    else
+        start_control_process
+    fi
 
-    # Get the number of servers
-    server_count=$(echo "$servers" | yq eval '. | length' -)
+    local servers=$(yq eval '.service.clustering.servers' $QTOOLS_CONFIG_FILE)
+    local server_count=$(echo "$servers" | yq eval '. | length' -)
 
-    SERVER_CORE_INDEX_START=0
-    SERVER_CORE_INDEX_END=0
-    # Loop through each server
-    for ((i=0; i<server_count; i++)); do
-        server=$(echo "$servers" | yq eval ".[$i]" -)
-        ip=$(echo "$server" | yq eval '.ip' -)
-        dataworker_count=$(echo "$server" | yq eval '.dataworker_count // "false"' -)
-        
-        # Skip invalid entries
-        if [ -z "$ip" ] || [ "$ip" == "null" ]; then
-            echo "Skipping invalid server entry: $server"
-            continue
-        fi
+    for ((i=0; i<$server_count; i++)); do
+        local server=$(yq eval ".service.clustering.servers[$i]" $QTOOLS_CONFIG_FILE)
+        local ip=$(echo "$server" | yq eval '.ip' -)
+        local dataworker_count=$(echo "$server" | yq eval '.dataworker_count' -)
+        local index_start=$(echo "$server" | yq eval '.index_start' -)
 
-        echo "Processing server: $ip"
-        echo "Server: $ip, Dataworker count: $dataworker_count"
-        
-        if echo "$(hostname -I)" | grep -q "$ip"; then
-            yq eval -i ".service.clustering.main_ip = \"$ip\"" $QTOOLS_CONFIG_FILE
-            echo "Set main IP to $ip in clustering configuration"
-            # This is the master server, so subtract 1 from the total core count
-            available_cores=$(($(nproc) - 1))
-            if [ "$dataworker_count" == "false" ]; then
-                dataworker_count=$(($(nproc) - 1))
-            fi
-
-            # Convert dataworker_count to integer and ensure it's not greater than available cores
-            dataworker_count=$(echo "$dataworker_count" | tr -cd '0-9')
-            dataworker_count=$((dataworker_count > 0 ? dataworker_count : available_cores))
-            dataworker_count=$((dataworker_count < available_cores ? dataworker_count : available_cores))
-        else
-            # Get the number of available cores
-            available_cores=$(ssh -i ~/.ssh/cluster-key "$ip" nproc)
-            
-            # Convert dataworker_count to integer and ensure it's not greater than available cores
-            dataworker_count=$(echo "$dataworker_count" | tr -cd '0-9')
-            dataworker_count=$((dataworker_count > 0 ? dataworker_count : available_cores))
-            dataworker_count=$((dataworker_count < available_cores ? dataworker_count : available_cores))
-        fi
-
-        echo "Dataworker count for $ip: $dataworker_count"
-        
-        # Increment the global count
-        TOTAL_EXPECTED_DATAWORKERS=$((TOTAL_EXPECTED_DATAWORKERS + dataworker_count))
-
-        # Create temporary YAML file with dataworkerMultiaddrs
-       
-        for ((j=0; j<dataworker_count; j++)); do
-            port=$((40000 + j + SERVER_CORE_INDEX_START))
-            if [ "$DRY_RUN" == "false" ]; then
-                yq eval -i ".engine.dataWorkerMultiaddrs += \"/ip4/$ip/tcp/$port\"" "$QUIL_CONFIG_FILE"
-            else
-                echo "Dry run, skipping adding dataworker multiaddr: /ip4/$ip/tcp/$port"
-            fi
-            SERVER_CORE_INDEX_END=$((SERVER_CORE_INDEX_END + 1))
-        done
-        
         if ! echo "$(hostname -I)" | grep -q "$ip"; then
-            # SCP the temporary file to the remote server
-            echo "Copying dataworker config to $ip"
             if [ "$DRY_RUN" == "false" ]; then
-                # Ensure the destination directory exists on the remote server
-                ssh -i ~/.ssh/cluster-key "$ip" "mkdir -p $HOME/ceremonyclient/node/.config"
-                scp -i ~/.ssh/cluster-key "$QUIL_CONFIG_FILE" "$ip:$HOME/ceremonyclient/node/.config/config.yml"
+                start_remote_cores "$ip" "$index_start" "$dataworker_count" &
             else
-                echo "Dry run, skipping copying dataworker config to $ip"
-            fi
-            
-            echo "Copying QTools config to $ip"
-            # SCP the QTools config to the remote server
-            if [ "$DRY_RUN" == "false" ]; then
-                scp -i ~/.ssh/cluster-key "$QTOOLS_CONFIG_FILE" "$ip:$HOME/qtools/config.yml"
-                start_remote_cores "$ip" "$SERVER_CORE_INDEX_START" &
-            else
-                log "Dry run, skipping starting qtools config on $ip"
-                log "Going to start $dataworker_count dataworkers on $ip with core index start $SERVER_CORE_INDEX_START"
+                echo -e "${BLUE}${INFO_ICON} [DRY RUN] Start cores on $ip with index start \
+                    of $index_start and dataworker count of $dataworker_count${RESET}"
             fi
         fi
-
-        SERVER_CORE_INDEX_START=$((SERVER_CORE_INDEX_END))
     done
-
-    # Print out the number of dataworker multiaddrs
-    actual_dataworkers=$(yq eval '.engine.dataWorkerMultiaddrs | length' "$QUIL_CONFIG_FILE")
-
-    if [ "$TOTAL_EXPECTED_DATAWORKERS" -ne "$actual_dataworkers" ]; then
-        echo -e "\e[33mWarning: The number of dataworker multiaddrs in the config doesn't match the expected count.\e[0m"
-        echo -e "${BLUE}${INFO_ICON} Dataworkers to be started: $TOTAL_EXPECTED_DATAWORKERS${RESET}"
-        echo -e "${BLUE}${INFO_ICON} Actual dataworker multiaddrs in config: $actual_dataworkers${RESET}"
-    else
-        echo -e "${BLUE}${INFO_ICON} Number of actual dataworkers found ($actual_dataworkers) matches the expected amount.${RESET}"
-    fi
 fi
-
-# Start the master if specified
-if [ "$MASTER" == "true" ]; then
-    if [ "$DRY_RUN" == "true" ]; then
-        echo -e "${BLUE}${INFO_ICON} [DRY RUN] Would start $QUIL_SERVICE_NAME.service${RESET}"
-    else
-        echo -e "${BLUE}${INFO_ICON} Starting $QUIL_SERVICE_NAME.service${RESET}"
-        sudo systemctl enable $QUIL_SERVICE_NAME.service
-        sudo systemctl start $QUIL_SERVICE_NAME.service
-    fi
-fi
-
 
 # Add a final message for dry run
-if [ "$DRY_RUN" == "true" ]; then
-    echo -e "\n${BLUE}${INFO_ICON} Dry run completed. No actual changes were made.${RESET}"
-else
-    start_local_cores $INDEX_START  
+echo -e "\n${BLUE}${INFO_ICON} Starting cores on local machine${RESET}"
+if [ "$DRY_RUN" == "false" ]; then
+    start_local_cores $INDEX_START $DATA_WORKER_COUNT
 fi
--- utils/clustering.sh
+++ utils/clustering.sh
@@ -7,6 +7,83 @@ is_master() {
     fi
 }
 
+create_dataworker_service_file() {
+    local BINARY=$(get_versioned_node)
+    local service_file="/etc/systemd/system/$QUIL_SERVICE_NAME-dataworker@.service"
+    if [ "$DRY_RUN" == "true" ]; then
+        echo -e "${BLUE}${INFO_ICON} [DRY RUN] Would create $service_file if it doesn't exist${RESET}"
+    else
+        USER=$(whoami)
+        GROUP=$(id -gn)
+        if [ -z "$USER" ] || [ -z "$GROUP" ]; then
+            echo "Error: Failed to get user or group information"
+            exit 1
+        fi
+        echo -e "${BLUE}${INFO_ICON} Creating $QUIL_SERVICE_NAME-dataworker@.service file...${RESET}"
+        sudo tee "$service_file" > /dev/null <<EOF
+[Unit]
+Description=Quilibrium Dataworker Service
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=50ms
+User=$USER
+Group=$GROUP
+WorkingDirectory=$(yq '.service.working_dir // "'$QUIL_NODE_PATH'"' $QTOOLS_CONFIG_FILE)
+ExecStart=$QUIL_NODE_PATH/$BINARY --core %i
+
+
+[Install]
+WantedBy=multi-user.target
+EOF
+        sudo systemctl daemon-reload
+        echo -e "${BLUE}${INFO_ICON} Service file created and systemd reloaded.${RESET}"
+    fi
+}
+
+start_control_process() {
+    echo -e "${BLUE}${INFO_ICON} Starting $QUIL_SERVICE_NAME.service${RESET}"
+    sudo systemctl enable $QUIL_SERVICE_NAME.service
+    sudo systemctl start $QUIL_SERVICE_NAME.service
+}
+
+# Function to start a single core
+start_core() {
+    local CORE=$1
+    if [ "$DRY_RUN" == "true" ]; then
+        echo -e "${BLUE}${INFO_ICON} [DRY RUN] Would enable and start $QUIL_SERVICE_NAME-dataworker@$CORE.service${RESET}"
+    else
+        if ! sudo systemctl start $QUIL_SERVICE_NAME-dataworker@$CORE.service; then
+            echo -e "\e[31mFailed to start $QUIL_SERVICE_NAME-dataworker@$CORE.service.\e[0m"
+        else
+            echo -e "\e[32mStarted $QUIL_SERVICE_NAME-dataworker@$CORE.service\e[0m"
+            sudo systemctl enable $QUIL_SERVICE_NAME-dataworker@$CORE.service
+        fi
+    fi
+}
+
+
+start_local_cores() {
+    local CORE_INDEX_START=$1
+    local CORE_COUNT=$2
+    echo -e "${BLUE}${INFO_ICON} Starting cluster's dataworkers on local machine${RESET}"
+    for ((i=0; i<CORE_COUNT; i++)); do
+        CORE=$((INDEX_START + i))
+        start_core $CORE &
+    done
+}
+
+start_remote_cores() {
+    local IP=$1
+    local CORE_INDEX_START=$2  
+    local CORE_COUNT=$3
+    echo -e "${BLUE}${INFO_ICON} Starting cluster's dataworkers on $IP${RESET}"
+    ssh -i ~/.ssh/cluster-key "$IP" "qtools start-cluster \
+        --index-start $CORE_INDEX_START \
+        --data-worker-count $CORE_COUNT"
+}
+
 get_cluster_server_info() {
     local servers=$(yq eval '.service.clustering.servers' $QTOOLS_CONFIG_FILE)
     local server_count=$(echo "$servers" | yq eval '. | length' -)
@@ -27,5 +104,104 @@ get_cluster_server_info() {
         fi
     done
     echo "$current_ip $current_dataworker_count $current_index_start"
+}
+
+update_quil_config() {
+    local DRY_RUN=${1:-false}
+    config=$(yq eval . $QTOOLS_CONFIG_FILE)
+    
+    # Get the array of servers
+    servers=$(echo "$config" | yq eval '.service.clustering.servers' -)
+
+    # Clear the existing dataworkerMultiaddrs array
+    yq eval -i '.engine.dataWorkerMultiaddrs = []' "$QUIL_CONFIG_FILE"
+
+    # Initialize TOTAL_EXPECTED_DATAWORKERS
+    TOTAL_EXPECTED_DATAWORKERS=0
+
+    # Get the number of servers
+    server_count=$(echo "$servers" | yq eval '. | length' -)
+
+    SERVER_CORE_INDEX_START=0
+    SERVER_CORE_INDEX_END=0
+    # Loop through each server
+    for ((i=0; i<server_count; i++)); do
+        server=$(echo "$servers" | yq eval ".[$i]" -)
+        ip=$(echo "$server" | yq eval '.ip' -)
+        dataworker_count=$(echo "$server" | yq eval '.dataworker_count // "false"' -)
+        
+        # Skip invalid entries
+        if [ -z "$ip" ] || [ "$ip" == "null" ]; then
+            echo "Skipping invalid server entry: $server"
+            continue
+        fi
+
+        echo "Processing server: $ip"
+        echo "Server: $ip, Dataworker count: $dataworker_count"
+        
+        if echo "$(hostname -I)" | grep -q "$ip"; then
+            yq eval -i ".service.clustering.main_ip = \"$ip\"" $QTOOLS_CONFIG_FILE
+            echo "Set main IP to $ip in clustering configuration"
+            # This is the master server, so subtract 1 from the total core count
+            available_cores=$(($(nproc) - 1))
+            if [ "$dataworker_count" == "false" ]; then
+                dataworker_count=$(($(nproc) - 1))
+            fi
+
+            # Convert dataworker_count to integer and ensure it's not greater than available cores
+            dataworker_count=$(echo "$dataworker_count" | tr -cd '0-9')
+            dataworker_count=$((dataworker_count > 0 ? dataworker_count : available_cores))
+            dataworker_count=$((dataworker_count < available_cores ? dataworker_count : available_cores))
+        else
+            # Get the number of available cores
+            available_cores=$(ssh -i ~/.ssh/cluster-key "$ip" nproc)
+            
+            # Convert dataworker_count to integer and ensure it's not greater than available cores
+            dataworker_count=$(echo "$dataworker_count" | tr -cd '0-9')
+            dataworker_count=$((dataworker_count > 0 ? dataworker_count : available_cores))
+            dataworker_count=$((dataworker_count < available_cores ? dataworker_count : available_cores))
+        fi
+
+        echo "Dataworker count for $ip: $dataworker_count"
+        
+        # Increment the global count
+        TOTAL_EXPECTED_DATAWORKERS=$((TOTAL_EXPECTED_DATAWORKERS + dataworker_count))
+
+        for ((j=0; j<dataworker_count; j++)); do
+            port=$((40000 + j + SERVER_CORE_INDEX_START))
+            if [ "$DRY_RUN" == "false" ]; then
+                yq eval -i ".engine.dataWorkerMultiaddrs += \"/ip4/$ip/tcp/$port\"" "$QUIL_CONFIG_FILE"
+            else
+                echo "Dry run, skipping adding dataworker multiaddr: /ip4/$ip/tcp/$port"
+            fi
+            SERVER_CORE_INDEX_END=$((SERVER_CORE_INDEX_END + 1))
+        done
+        
+
+        SERVER_CORE_INDEX_START=$((SERVER_CORE_INDEX_END))
+    done
+
+    # Print out the number of dataworker multiaddrs
+    actual_dataworkers=$(yq eval '.engine.dataWorkerMultiaddrs | length' "$QUIL_CONFIG_FILE")
+
+    if [ "$TOTAL_EXPECTED_DATAWORKERS" -ne "$actual_dataworkers" ]; then
+        echo -e "\e[33mWarning: The number of dataworker multiaddrs in the config doesn't match the expected count.\e[0m"
+        echo -e "${BLUE}${INFO_ICON} Dataworkers to be started: $TOTAL_EXPECTED_DATAWORKERS${RESET}"
+        echo -e "${BLUE}${INFO_ICON} Actual dataworker multiaddrs in config: $actual_dataworkers${RESET}"
+    else
+        echo -e "${BLUE}${INFO_ICON} Number of actual dataworkers found ($actual_dataworkers) matches the expected amount.${RESET}"
+    fi
+}
+
+copy_quil_config_to_server() {
+    local ip=$1
+
+    scp -i ~/.ssh/cluster-key "$QUIL_CONFIG_FILE" "$ip:$HOME/ceremonyclient/node/.config/config.yml"
+}
+
+copy_qtools_config_to_server() {
+    local ip=$1
+
+    scp -i ~/.ssh/cluster-key "$QTOOLS_CONFIG_FILE" "$ip:$HOME/qtools/config.yml"
 }
 
