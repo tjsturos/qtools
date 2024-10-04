0ca18211b98524b97aea763c5cc9fa0f2a3c9bc4
Tyler Sturos <tyler.john@qcommander.sh>
2024-10-03 18:04:59 -0800

    change core count

--- scripts/service-commands/stop.sh
+++ scripts/service-commands/stop.sh
@@ -81,7 +81,8 @@ if [ "$IS_CLUSTERING_ENABLED" == "true" ] && echo "$(hostname -I)" | grep -q "$M
     server_count=$(echo "$servers" | yq eval '. | length' -)
 
     core_index=1
-    for ((i=0; i<$(nproc - 1); i++)); do
+    local_core_count=$(nproc - 1)
+    for ((i=0; i<$local_core_count; i++)); do
         stop_core $(($i + $core_index)) &
         core_index=$(($core_index + 1))
     done
