aafafa30a8c31a6bbeb081678e1d36334377ef2a
Tyler Sturos <tyler.john@qcommander.sh>
2024-10-03 18:25:22 -0800

    do not install certain scheduled tasks on non-master nodes

--- scripts/install/install-cron.sh
+++ scripts/install/install-cron.sh
@@ -2,6 +2,9 @@
 # HELP: Installs all the automated taks for this node.  Currently, it runs qtools & node updates, as well as backups (if enabled) every 10 minutes.  It also records the unclaimed balances on different intervals (every hour, 1x a day, 1x week, 1x month).
 log "Updating this user's crontab for automated tasks..."
 
+IS_CLUSTERING_ENABLED="$(yq '.service.clustering.enabled // false' $QTOOLS_CONFIG_FILE)"
+IS_MASTER="$(is_master)"
+
 # The aim of this is to get the current cron tasks file, add the line to run the on-start.sh script every reboot.
 FILE_CRON=$QTOOLS_PATH/cron
 FILE_ACTUAL_OUTPUT="actual.txt"
@@ -42,41 +45,44 @@ if [ "$AUTO_UPDATE_QTOOLS" == "true" ]; then
   append_to_file $FILE_CRON "$QTOOLS_UPDATE_CRON_EXPRESSION qtools self-update --auto" false
 fi
 
-AUTO_RUN_DIAGNOSTICS=$(yq eval '.scheduled_tasks.diagnostics.enabled // false' $QTOOLS_CONFIG_FILE)
+if [ "$IS_CLUSTERING_ENABLED" == "true" ] && [ "$IS_MASTER" == "true" ]; then
+  AUTO_RUN_DIAGNOSTICS=$(yq eval '.scheduled_tasks.diagnostics.enabled // false' $QTOOLS_CONFIG_FILE)
 
-if [ "$AUTO_RUN_DIAGNOSTICS" == "true" ]; then
-  DIAGNOSTICS_CRON_EXPRESSION=$(yq eval '.scheduled_tasks.diagnostics.cron_expression // "*/10 * * * *"' $QTOOLS_CONFIG_FILE)
-  
-  # Check if Expression is valid
-  if [ -z "$DIAGNOSTICS_CRON_EXPRESSION" ]; then
-    log "Error: Diagnostics cron expression is empty. Using default value."
-    DIAGNOSTICS_CRON_EXPRESSION="*/10 * * * *"
+  if [ "$AUTO_RUN_DIAGNOSTICS" == "true" ] ; then
+    DIAGNOSTICS_CRON_EXPRESSION=$(yq eval '.scheduled_tasks.diagnostics.cron_expression // "*/10 * * * *"' $QTOOLS_CONFIG_FILE)
+    
+    # Check if Expression is valid
+    if [ -z "$DIAGNOSTICS_CRON_EXPRESSION" ]; then
+      log "Error: Diagnostics cron expression is empty. Using default value."
+      DIAGNOSTICS_CRON_EXPRESSION="*/10 * * * *"
+    fi
+    
+    append_to_file $FILE_CRON "$DIAGNOSTICS_CRON_EXPRESSION qtools run-diagnostics --auto" false
   fi
-  
-  append_to_file $FILE_CRON "$DIAGNOSTICS_CRON_EXPRESSION qtools run-diagnostics --auto" false
-fi
 
-AUTO_BACKUP_STORE=$(yq eval '.scheduled_tasks.backup.enabled // false' $QTOOLS_CONFIG_FILE)
+  AUTO_BACKUP_STORE=$(yq eval '.scheduled_tasks.backup.enabled // false' $QTOOLS_CONFIG_FILE)
 
-if [ "$AUTO_BACKUP_STORE" == "true" ]; then
-  BACKUP_STORE_CRON_EXPRESSION=$(yq eval '.scheduled_tasks.backup.cron_expression // "*/10 * * * *"' $QTOOLS_CONFIG_FILE)
-  
-  # Check if Expression is valid
-  if [ -z "$BACKUP_STORE_CRON_EXPRESSION" ]; then
-    log "Error: Backup store cron expression is empty. Using default value."
-    BACKUP_STORE_CRON_EXPRESSION="*/10 * * * *"
+  if [ "$AUTO_BACKUP_STORE" == "true" ]; then
+    BACKUP_STORE_CRON_EXPRESSION=$(yq eval '.scheduled_tasks.backup.cron_expression // "*/10 * * * *"' $QTOOLS_CONFIG_FILE)
+    
+    # Check if Expression is valid
+    if [ -z "$BACKUP_STORE_CRON_EXPRESSION" ]; then
+      log "Error: Backup store cron expression is empty. Using default value."
+      BACKUP_STORE_CRON_EXPRESSION="*/10 * * * *"
+    fi
+    
+    append_to_file $FILE_CRON "$BACKUP_STORE_CRON_EXPRESSION qtools backup-store" false
   fi
-  
-  append_to_file $FILE_CRON "$BACKUP_STORE_CRON_EXPRESSION qtools backup-store" false
-fi
 
-STATS_ENABLED=$(yq eval '.scheduled_tasks.statistics.enabled // false' $QTOOLS_CONFIG_FILE)
+  STATS_ENABLED=$(yq eval '.scheduled_tasks.statistics.enabled // false' $QTOOLS_CONFIG_FILE)
 
-if [ "$STATS_ENABLED" == "true" ]; then
-  append_to_file $FILE_CRON '0 * * * * qtools record-unclaimed-rewards hourly' false
-  append_to_file $FILE_CRON '0 0 * * * qtools record-unclaimed-rewards daily' false
-  append_to_file $FILE_CRON '0 0 * * 0 qtools record-unclaimed-rewards weekly' false
-  append_to_file $FILE_CRON '0 0 1 * * qtools record-unclaimed-rewards monthly' false
+  if [ "$STATS_ENABLED" == "true" ]; then
+    append_to_file $FILE_CRON '0 * * * * qtools record-unclaimed-rewards hourly' false
+    append_to_file $FILE_CRON '0 0 * * * qtools record-unclaimed-rewards daily' false
+    append_to_file $FILE_CRON '0 0 * * 0 qtools record-unclaimed-rewards weekly' false
+    append_to_file $FILE_CRON '0 0 1 * * qtools record-unclaimed-rewards monthly' false
+  fi
+
 fi
 
 
