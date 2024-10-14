b3105d87b3812257b14861c79460c3b228a65cc2
Tyler Sturos <tyler.john@qcommander.sh>
2024-10-14 13:55:51 -0800

    loop through in linear order

--- scripts/shortcuts/qclient-dry-run-merge.sh
+++ scripts/shortcuts/qclient-dry-run-merge.sh
@@ -38,16 +38,14 @@ log_result() {
 # Loop through increasing number of folders
 for ((num_folders=1; num_folders<=${#folders[@]}; num_folders++)); do
     # Use combination to generate all possible combinations of folders
-    combinations=$(seq 0 $((${#folders[@]} - 1)) | awk -v k=$num_folders 'BEGIN{OFS=","}{for(i=1;i<=k;i++)printf "%s%s",$(i+NR-1),i==k?ORS:OFS}' | tr ',' ' ')
-    
-    while read -r combination; do
-        selected_folders=()
-        for index in $combination; do
-            selected_folders+=("${folders[index]}")
-        done
-        echo -e "Command used:\n./qclient-2.0.0-$OS_ARCH config prover merge $QUIL_NODE_PATH/.config/ "${selected_folders[@]}" --dry-run"
-        score=$(run_merge "${selected_folders[@]}")
-        log_result "$score" "${selected_folders[@]}"
-    done <<< "$combinations"
+
+    selected_folders=()
+    for ((i=0; i<num_folders; i++)); do
+        selected_folders+=("${folders[i]}")
+    done
+    echo -e "Command used:\n./qclient-2.0.0-$OS_ARCH config prover merge $QUIL_NODE_PATH/.config/ "${selected_folders[@]}" --dry-run"
+    score=$(run_merge "${selected_folders[@]}")
+    log_result "$score" "${selected_folders[@]}"
+ 
 done
 
