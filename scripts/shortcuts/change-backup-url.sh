14a205b41942395657f8d546d95ca88f4c661e9f
Tyler Sturos <tyler.john@qcommander.sh>
2024-10-14 11:53:43 -0800

    change backup url

---
+++ scripts/shortcuts/change-backup-url.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <new_url>"
+    exit 1
+fi
+log "Changing backup URL to $1"
+yq eval -i ".scheduled_tasks.backup.backup_url = \"$1\"" $QTOOLS_CONFIG_FILE
+
