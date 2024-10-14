e196850197f319f69f80d8bdf6946007b26eda05
Tyler Sturos <tyler.john@qcommander.sh>
2024-10-14 13:41:24 -0800

    update spacing on prints

--- scripts/shortcuts/qclient-dry-run-merge.sh
+++ scripts/shortcuts/qclient-dry-run-merge.sh
@@ -32,7 +32,7 @@ log_result() {
     local folders=("$@")
     local score=$1
     shift
-    echo -e "\nFolders: ${folders[*]} - Score: $score"
+    echo -e "Folders: ${folders[*]} - Score: $score"
 }
 
 # Loop through increasing number of folders
@@ -45,7 +45,7 @@ for ((num_folders=1; num_folders<=${#folders[@]}; num_folders++)); do
         for index in $combination; do
             selected_folders+=("${folders[index]}")
         done
-        echo -e "\nCommand used: ./qclient-2.0.0-$OS_ARCH config prover merge $QUIL_NODE_PATH/.config/ "${selected_folders[@]}" --dry-run\n"
+        echo -e "Command used:\n./qclient-2.0.0-$OS_ARCH config prover merge $QUIL_NODE_PATH/.config/ "${selected_folders[@]}" --dry-run"
         score=$(run_merge "${selected_folders[@]}")
         log_result "$score" "${selected_folders[@]}"
     done <<< "$combinations"
