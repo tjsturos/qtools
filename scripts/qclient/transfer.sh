66f61a9a30c768f7ac3f37403b93acbe5ce6f139
Tyler Sturos <tyler.john@qcommander.sh>
2024-10-26 15:25:54 -0800

    add additional log

--- scripts/qclient/transfer.sh
+++ scripts/qclient/transfer.sh
@@ -78,6 +78,7 @@ fi
 
 # Check if TOKEN is blank
 if [ -z "$TOKEN" ]; then
+    echo "No token specified, selecting from available tokens..."
     TOKEN=$(get_token_from_user_input $CONFIG_PATH $SKIP_SIG_CHECK)
 fi
 
