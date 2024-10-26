1bdf43d5ae20319f6da8893e67e3b227e15775ac
Tyler Sturos <tyler.john@qcommander.sh>
2024-10-26 15:30:52 -0800

    update command

--- scripts/qclient/utils.sh
+++ scripts/qclient/utils.sh
@@ -97,12 +97,12 @@ get_token_from_user_input() {
     local SKIP_SIG_CHECK="$2"
     local TOKENS=$(get_tokens $CONFIG_PATH $SKIP_SIG_CHECK)
 
-    echo "Tokens: $TOKENS"
     # Create an array to store the tokens
     TOKEN_ARRAY=()
     
     # Populate the array with tokens
     while IFS= read -r line; do
+        echo "Token found: $line"
         TOKEN_ARRAY+=("$line")
     done <<< "$TOKENS"
     
