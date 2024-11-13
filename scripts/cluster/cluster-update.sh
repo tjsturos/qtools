62ba4316964e5037225ed155276fa0c374ce7abe
Tyler Sturos <tyler.john@qcommander.sh>
2024-11-13 13:28:28 -0900

    set dry run to false for update cluster

--- scripts/cluster/cluster-update.sh
+++ scripts/cluster/cluster-update.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 IS_MASTER=false
+DRY_RUN=false
+
 # Check if --master option is passed
 if [[ "$*" == *"--master"* ]]; then
    IS_MASTER=true
+fi
+
+# Check if --dry-run option is passed
+if [[ "$*" == *"--dry-run"* ]]; then
+   DRY_RUN=true
 fi
 
 # Main script execution
