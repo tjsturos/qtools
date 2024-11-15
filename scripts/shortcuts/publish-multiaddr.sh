fba4acb259714c85e56214301a32eb4ad58d1282
Tyler Sturos <tyler.john@qcommander.sh>
2024-11-14 19:20:34 -0900

    publish multiaddr to central list

---
+++ scripts/shortcuts/publish-multiaddr.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Get publish multiaddr settings from config
+SSH_KEY_PATH=$(yq eval '.settings.publish_multiaddr.ssh_key_path' $QTOOLS_CONFIG_FILE)
+REMOTE_USER=$(yq eval '.settings.publish_multiaddr.remote_user' $QTOOLS_CONFIG_FILE)
+REMOTE_HOST=$(yq eval '.settings.publish_multiaddr.remote_host' $QTOOLS_CONFIG_FILE)
+REMOTE_FILE=$(yq eval '.settings.publish_multiaddr.remote_file' $QTOOLS_CONFIG_FILE)
+
+# Get the multiaddr
+MULTIADDR=$(qtools get-multiaddr)
+
+# Create or update the remote YAML file
+ssh -i "$SSH_KEY_PATH" "${REMOTE_USER}@${REMOTE_HOST}" "
+if [ ! -f $REMOTE_FILE ]; then
+    echo 'directPeers: []' > $REMOTE_FILE
+fi
+
+# Check if multiaddr already exists
+if ! grep -q \"$MULTIADDR\" $REMOTE_FILE; then
+    # Add new multiaddr to directPeers array
+    yq eval -i '.directPeers += [\"$MULTIADDR\"]' $REMOTE_FILE
+    echo 'Multiaddr added successfully'
+else
+    echo 'Multiaddr already exists in the list'
+fi
+"
