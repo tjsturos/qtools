90f97407551573796c01bb023f03aa0870704fd4
Tyler Sturos <tyler.john@qcommander.sh>
2024-11-14 19:10:36 -0900

    get multiaddr of server

---
+++ scripts/shortcuts/get-multiaddr.sh
@@ -0,0 +1,12 @@
+
+
+PUBLIC_IP=$(curl https://ipecho.net/plain ; echo)
+LISTEN_MODE=$(yq eval '.settings.listenAddr.mode' $QTOOLS_CONFIG_FILE)
+LISTEN_PORT=$(yq eval '.settings.listenAddr.port' $QTOOLS_CONFIG_FILE)
+PEER_ID=$(qtools peer-id)
+PROTOCOL=""
+if [ "$LISTEN_MODE" = "udp" ]; then
+    PROTOCOL="/quic-v1"
+fi
+
+echo "/ip4/${PUBLIC_IP}/${LISTEN_MODE}/${LISTEN_PORT}${PROTOCOL}/p2p/${PEER_ID}"
