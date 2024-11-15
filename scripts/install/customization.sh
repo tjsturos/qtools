0ba236f4a66483d5ecb6ab76d27e6443a980f4f5
Tyler Sturos <tyler.john@qcommander.sh>
2024-11-14 18:28:25 -0900

    add ipv6 disabling

--- scripts/install/customization.sh
+++ scripts/install/customization.sh
@@ -6,5 +6,10 @@ sudo mv /etc/apt/apt.conf.d/20apt-esm-hook.conf /etc/apt/apt.conf.d/20apt-esm-ho
 FILE_SYSCTL=/etc/sysctl.conf
 append_to_file $FILE_SYSCTL "net.core.rmem_max = 600000000" false
 append_to_file $FILE_SYSCTL "net.core.wmem_max = 600000000" false
+append_to_file $FILE_SYSCTL "net.ipv4.tcp_fastopen = 3" false
+append_to_file $FILE_SYSCTL "net.ipv6.conf.all.disable_ipv6 = 1" false
+append_to_file $FILE_SYSCTL "net.ipv6.conf.default.disable_ipv6 = 1" false
+append_to_file $FILE_SYSCTL "net.ipv6.conf.lo.disable_ipv6 = 1" false
+
 # load the updates
 sudo sysctl -p
