6c793bc77da06c299a4c75fb59e293add40f6756
Tyler Sturos <tyler.john@qcommander.sh>
2024-11-17 20:54:06 -0900

    add flags for manual direction

--- scripts/shortcuts/toggle-hyperthreading.sh
+++ scripts/shortcuts/toggle-hyperthreading.sh
@@ -1,10 +1,40 @@
 
 CURRENT_STATE=$(cat /sys/devices/system/cpu/smt/control)
 
-if [ "$CURRENT_STATE" != "on" ]; then
-    echo "Disabling hyperthreading"
-    sudo sh -c 'echo "on" > /sys/devices/system/cpu/smt/control'
-else
+MANUAL_STATE=""
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --on)
+            MANUAL_STATE="on"
+            shift
+            ;;
+        --off)
+            MANUAL_STATE="off" 
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+enable_hyperthreading() {
     echo "Enabling hyperthreading"
+    sudo sh -c 'echo "on" > /sys/devices/system/cpu/smt/control'
+}
+
+disable_hyperthreading() {
+    echo "Disabling hyperthreading"
     sudo sh -c 'echo "off" > /sys/devices/system/cpu/smt/control'
+}
+
+if [ "$MANUAL_STATE" == "on" ]; then        
+    enable_hyperthreading
+elif [ "$MANUAL_STATE" == "off" ]; then
+    disable_hyperthreading
+elif [ "$CURRENT_STATE" != "on" ]; then
+    enable_hyperthreading
+else
+    disable_hyperthreading
 fi
