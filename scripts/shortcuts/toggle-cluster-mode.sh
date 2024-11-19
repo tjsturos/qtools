dfa948c3c6eff20b69b309a9e1b784af2a0b5e24
Tyler Sturos <tyler.john@qcommander.sh>
2024-11-18 23:43:14 -0900

    add clustering toggle

---
+++ scripts/shortcuts/toggle-cluster-mode.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# HELP: Toggles cluster mode on/off in the qtools config file
+
+qtools cluster-mode "$@"
---
+++ scripts/toggle/cluster-mode.sh
@@ -0,0 +1,41 @@
+# Parse command line arguments
+NEW_MODE=""
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --on)
+            NEW_MODE="true"
+            shift
+            ;;
+        --off) 
+            NEW_MODE="false"
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            echo "Usage: qtools cluster-mode [--on|--off]"
+            exit 1
+            ;;
+    esac
+done
+
+# If no flag provided, toggle current value
+if [ -z "$NEW_MODE" ]; then
+    CURRENT_MODE=$(yq '.service.clustering.enabled // false' $QTOOLS_CONFIG_FILE)
+    if [ "$CURRENT_MODE" == "true" ]; then
+        NEW_MODE="false"
+    else
+        NEW_MODE="true" 
+    fi
+fi
+
+# Update the config file
+yq -i '.service.clustering.enabled = '$NEW_MODE'' $QTOOLS_CONFIG_FILE
+
+if [ "$NEW_MODE" == "false" ]; then
+    qtools cluster-stop
+else 
+    qtools cluster-setup --master
+fi
+
+
+log "Cluster mode has been set to: $NEW_MODE"
