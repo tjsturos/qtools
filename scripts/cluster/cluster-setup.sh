e02e2c6d4131010ee92dda5e22903fa4282cc030
Tyler Sturos <tyler.john@qcommander.sh>
2024-12-09 14:55:53 -0900

    reduce logic

--- scripts/cluster/cluster-setup.sh
+++ scripts/cluster/cluster-setup.sh
@@ -236,7 +236,7 @@ handle_server() {
 
     local IS_LOCAL_SERVER=$(echo "$(hostname -I)" | grep "$SERVER_IP")
 
-    if [[ $LOCAL_ONLY != "true" ]] && [ -n "$IS_LOCAL_SERVER" ]; then
+    if [ -n "$IS_LOCAL_SERVER" ]; then
         if ! check_server_ssh_connection $SERVER_IP $REMOTE_USER $SSH_PORT; then
             echo -e "${RED}${WARNING_ICON} Failed to connect to $SERVER_IP ($REMOTE_USER) on port $SSH_PORT${RESET}"
             echo -e "${BLUE}${INFO_ICON} Skipping server setup for $SERVER_IP ($REMOTE_USER)${RESET}"
