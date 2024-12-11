42ffb652fbd203de1095c5fbec01066a22313fe6
Tyler Sturos <tyler.john@qcommander.sh>
2024-12-11 14:03:34 -0900

    update status check for data worker only machine

--- scripts/service-commands/status.sh
+++ scripts/service-commands/status.sh
@@ -2,5 +2,35 @@
 # HELP: Gets the node application service\'s current status.
 # Usage: qtools status
 
-sudo systemctl status $QUIL_SERVICE_NAME.service --no-pager
+WORKER_NUM=""
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --worker)
+            shift
+            WORKER_NUM="$1"
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+
+
+
+if [ "$IS_CLUSTERING_ENABLED" == "true" ]; then
+    if [ "$IS_MASTER" == "true" ]; then
+        sudo systemctl status $QUIL_SERVICE_NAME.service --no-pager
+    else
+        if [ ! -z "$WORKER_NUM" ]; then
+            sudo systemctl status "$QUIL_DATA_WORKER_SERVICE_NAME@$WORKER_NUM.service" --no-pager
+        else
+            echo "Is not the master, check status with --worker <int>"
+        fi
+    fi
+else
+    sudo systemctl status $QUIL_SERVICE_NAME.service --no-pager
+fi
 
