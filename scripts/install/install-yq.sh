4bf40c7aeacb8fe60e196ad1522966e37dcad4e4
Tyler Sturos <tyler.john@qcommander.sh>
2025-01-04 20:54:44 -0900

    support arm

--- scripts/install/install-yq.sh
+++ scripts/install/install-yq.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # HELP: Installs \'yq\' version 4.20.1 for parsing config files.
 VERSION=v4.44.2
-BINARY=yq_linux_amd64
+if [[ "$OS_ARCH" == *"arm64"* ]]; then
+    BINARY=yq_linux_arm64
+    VERSION=v4.44.6
+else
+    BINARY=yq_linux_amd64
+    VERSION=v4.44.2
+fi
 cd $QTOOLS_PATH/binaries
 
 COMPRESSED_FILENAME=${BINARY}_${VERSION}.tar.gz
