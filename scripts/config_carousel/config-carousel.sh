3fe079f3903e38dc9c46edc83910890e1d2fcfd8
Tyler Sturos <tyler.john@qcommander.sh>
2025-01-23 19:29:47 -0900

    add || statement

--- scripts/config_carousel/config-carousel.sh
+++ scripts/config_carousel/config-carousel.sh
@@ -80,7 +80,7 @@ if [ "$DAEMON_MODE" = true ]; then
         while [ $FRAME_COUNT -lt $FRAMES_BEFORE_SWITCH ]; do
             while read -r line; do
                 # When starting new frame batch, mark worker as in-use
-                if [[ $line =~ "creating data shard ring proof" ]]; then
+                if [[ $line =~ "creating data shard ring proof" ]] || [[ $line =~ "peer announced but has already been announced" ]]; then
                     log_message "Marking worker as in-use"
                     qtools toggle-worker-availability --in-use
                 elif [[ $line =~ "submitting data proof" ]]; then
