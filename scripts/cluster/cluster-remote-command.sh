5ea02d261e25ae803a54309272cb2726aa6a3ed2
Tyler Sturos <tyler.john@qcommander.sh>
2025-01-24 18:51:24 -0900

    add filters for command

--- scripts/cluster/cluster-remote-command.sh
+++ scripts/cluster/cluster-remote-command.sh
@@ -3,13 +3,36 @@
 
 source $QTOOLS_PATH/scripts/cluster/utils.sh
 
-if [ $# -eq 0 ]; then
+# Parse command line arguments
+TARGET_SERVERS=""
+COMMAND=""
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --server)
+            TARGET_SERVERS="$2"
+            shift 2
+            ;;
+        --servers)
+            TARGET_SERVERS="$2"
+            shift 2
+            ;;
+        *)
+            if [ -z "$COMMAND" ]; then
+                COMMAND="$1"
+            else
+                COMMAND="$COMMAND $1"
+            fi
+            shift
+            ;;
+    esac
+done
+
+if [ -z "$COMMAND" ]; then
     echo "Error: No command provided"
-    echo "Usage: cluster-remote-command \"<command>\""
+    echo "Usage: cluster-remote-command [--server <ip4>] [--servers \"<ip4>|<ip4>\"] \"<command>\""
     exit 1
 fi
-
-COMMAND="$1"
 
 execute_remote_command() {
     local config=$(yq eval . $QTOOLS_CONFIG_FILE)
@@ -26,14 +49,23 @@ execute_remote_command() {
         local ssh_port=$(echo "$server" | yq eval ".ssh_port // \"$DEFAULT_SSH_PORT\"" -)
 
         # Skip if it's the current server
-        if ! echo "$(hostname -I)" | grep -q "$server_ip"; then
-            echo "Starting command execution on $server_ip..."
-            (
-                ssh_to_remote $server_ip $remote_user $ssh_port "$COMMAND"
-                echo "Command completed on $server_ip"
-            ) &
-            pids+=($!)
+        if echo "$(hostname -I)" | grep -q "$server_ip"; then
+            continue
         fi
+
+        # Skip if target servers are specified and this server is not in the list
+        if [ -n "$TARGET_SERVERS" ]; then
+            if ! echo "$TARGET_SERVERS" | grep -q "$server_ip"; then
+                continue
+            fi
+        fi
+
+        echo "Starting command execution on $server_ip..."
+        (
+            ssh_to_remote $server_ip $remote_user $ssh_port "$COMMAND"
+            echo "Command completed on $server_ip"
+        ) &
+        pids+=($!)
     done
 
     # Wait for all background processes to complete
